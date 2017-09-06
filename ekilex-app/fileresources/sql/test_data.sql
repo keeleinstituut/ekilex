67726bdb67403be2e3a09f76f4bd10e531c079f5
arvi <arvi@tavast.ee>
2017-09-06 17:31:51 +0300

    more test data

--- ekilex-app/fileresources/sql/test_data.sql
+++ ekilex-app/fileresources/sql/test_data.sql
@@ -12,6 +12,11 @@ insert into word (id, value, components, lang, morph_code) values (1010, 'tumeha
 insert into word (id, value, components, lang, morph_code) values (1011, 'hiirhall', '{hiir, hall}', 'est', 'SgN');
 insert into word (id, value, components, lang, morph_code) values (1012, 'hiirjas', '{hiirjas}', 'est', 'SgN');
 insert into word (id, value, components, lang, morph_code) values (1013, 'hiirukas', '{hiirukas}', 'est', 'SgN');
+insert into word (id, value, components, lang, morph_code) values (1014, 'väär', '{väär}', 'est', 'SgN');
+insert into word (id, value, components, lang, morph_code) values (1015, 'заморозки', '{заморозки}', 'rus', 'PlN');
+insert into word (id, value, components, lang, morph_code) values (1016, 'серый', '{серый}', 'rus', 'SgN');
+insert into word (id, value, components, lang, morph_code) values (1017, 'холл', '{холл}', 'rus', 'SgN');
+
 
 insert into meaning (id, dataset) values (2001, '{eos}');
 insert into meaning (id, dataset) values (2002, '{eos}');
@@ -38,6 +43,8 @@ insert into meaning (id, dataset) values (2022, '{eos}');
 insert into meaning (id, dataset) values (2023, '{eos}');
 insert into meaning (id, dataset) values (2024, '{eos}');
 insert into meaning (id, dataset) values (2025, '{eos}');
+insert into meaning (id, dataset) values (2026, '{eos}');
+insert into meaning (id, dataset) values (2027, '{eos}');
 
 insert into definition (meaning_id, value, dataset) values (2001, 'külmunud kaste maas', '{eos}');
 insert into definition (meaning_id, value, dataset) values (2002, 'avar üldkasutatav hoone v ruum; suur esinduslik ruum elamus, kodasaal', '{eos}');
@@ -52,6 +59,7 @@ insert into definition (meaning_id, value, dataset) values (2011, 'rohkearvulise
 insert into definition (meaning_id, value, dataset) values (2016, 'suur esik; saal; tootmishoone', '{evs}');
 insert into definition (meaning_id, value, dataset) values (2021, 'suur selvekauplus, kus peale toiduainete müüakse ka muid igapäevakaupu', '{eos}');
 insert into definition (meaning_id, value, dataset) values (2025, 'hiirekarva hall', '{eos}');
+insert into definition (meaning_id, value, dataset) values (2026, 'rõdu, eriti kirikus', '{eos}');
 
 insert into morph_homonym (id, word_id) values (10001, 1002);
 insert into morph_homonym (id, word_id) values (10002, 1002);
@@ -66,20 +74,30 @@ insert into morph_homonym (id, word_id) values (10010, 1010);
 insert into morph_homonym (id, word_id) values (10011, 1011);
 insert into morph_homonym (id, word_id) values (10012, 1012);
 insert into morph_homonym (id, word_id) values (10013, 1013);
+insert into morph_homonym (id, word_id) values (10014, 1014);
+insert into morph_homonym (id, word_id) values (10015, 1014);
+insert into morph_homonym (id, word_id) values (10016, 1015);
+insert into morph_homonym (id, word_id) values (10017, 1016);
+insert into morph_homonym (id, word_id) values (10018, 1017);
+
+
 
 insert into paradigm (id, morph_homonym_id, display_form, example) values (11001, 10001, 'h`all', 'tark');
 insert into paradigm (id, morph_homonym_id, display_form, example) values (11002, 10002, 'h`al''l', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11003, 10003, 'hallakord', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11004, 10004, 'hallaudu', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11005, 10005, 'hallaöö', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11006, 10006, 'hallasääsk', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11007, 10007, 'kaubahall', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11008, 10008, 'linnahall', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11009, 10009, 'helehall', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11010, 10010, 'tumehall', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11011, 10011, 'hiirhall', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11012, 10012, 'hiirjas', 'kurt');
-insert into paradigm (id, morph_homonym_id, display_form, example) values (11013, 10013, 'hiirukas', 'kurt');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11003, 10003, 'hallakord', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11004, 10004, 'hallaudu', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11005, 10005, 'hallaöö', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11006, 10006, 'hallasääsk', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11007, 10007, 'kaubahall', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11008, 10008, 'linnahall', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11009, 10009, 'helehall', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11010, 10010, 'tumehall', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11011, 10011, 'hiirhall', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11012, 10012, 'hiirjas', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11013, 10013, 'hiirukas', '');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11014, 10014, 'v`äär', 'riik');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11015, 10015, 'v`äär', 'külm');
+insert into paradigm (id, morph_homonym_id, display_form, example) values (11016, 10015, 'v`äär', 'õpik');
 
 insert into form (paradigm_id, morph_code, value) values (11001, 'SgN', 'hall');
 insert into form (paradigm_id, morph_code, value) values (11001, 'SgG', 'halla');
@@ -87,6 +105,15 @@ insert into form (paradigm_id, morph_code, value) values (11001, 'SgP', 'h`alla'
 insert into form (paradigm_id, morph_code, value) values (11002, 'SgN', 'hall');
 insert into form (paradigm_id, morph_code, value) values (11002, 'SgG', 'halli');
 insert into form (paradigm_id, morph_code, value) values (11002, 'SgP', 'h`alli');
+insert into form (paradigm_id, morph_code, value) values (11014, 'SgN', 'v`äär');
+insert into form (paradigm_id, morph_code, value) values (11014, 'SgG', 'vääri');
+insert into form (paradigm_id, morph_code, value) values (11014, 'SgP', 'v`ääri');
+insert into form (paradigm_id, morph_code, value) values (11015, 'SgN', 'v`äär');
+insert into form (paradigm_id, morph_code, value) values (11015, 'SgG', 'väära');
+insert into form (paradigm_id, morph_code, value) values (11015, 'SgP', 'v`äära');
+insert into form (paradigm_id, morph_code, value) values (11016, 'SgN', 'v`äär');
+insert into form (paradigm_id, morph_code, value) values (11016, 'SgG', 'v`äära');
+insert into form (paradigm_id, morph_code, value) values (11016, 'SgP', 'v`äärat');
 
 insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4001, 10002, 2001, '{eos}');
 insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4002, 10002, 2002, '{eos}');
@@ -115,6 +142,11 @@ insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4024, 100
 insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4025, 10011, 2025, '{eos}');
 insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4026, 10012, 2025, '{eos}');
 insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4027, 10013, 2025, '{eos}');
+insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4028, 10014, 2026, '{eos}');
+insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4029, 10015, 2027, '{eos}');
+insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4030, 10015, 2014, '{evs}');
+insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4031, 10016, 2015, '{evs}');
+insert into lexeme (id, morph_homonym_id, meaning_id, dataset) values (4032, 10017, 2016, '{evs}');
 
 insert into lex_relation (lexeme1_id, lexeme2_id, lex_rel_type_code, dataset) values (4001, 4017, 'comp', '{eos}');
 insert into lex_relation (lexeme1_id, lexeme2_id, lex_rel_type_code, dataset) values (4001, 4018, 'comp', '{eos}');
