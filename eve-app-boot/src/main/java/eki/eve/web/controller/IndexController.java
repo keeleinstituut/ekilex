d1fe63f3ea2e551ee3be9fb48ecfc06ea9eb66ad
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-09-11 11:18:10 +0300

    Example how to set up logging and different settings for profiles (default and test).

--- eve-app-boot/src/main/java/eki/eve/web/controller/IndexController.java
+++ eve-app-boot/src/main/java/eki/eve/web/controller/IndexController.java
@@ -1,13 +1,18 @@
 package eki.eve.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping("/")
     public String index() {
+        logger.debug("Here are the dragons...");
         return "index";
     }
 
