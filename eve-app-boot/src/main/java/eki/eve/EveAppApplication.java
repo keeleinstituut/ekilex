4d34192ed57e2208c18df289fe52ae498c9916f0
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-09-11 17:30:07 +0300

    Remove unneeded annotation.

--- eve-app-boot/src/main/java/eki/eve/EveAppApplication.java
+++ eve-app-boot/src/main/java/eki/eve/EveAppApplication.java
@@ -2,10 +2,8 @@ package eki.eve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"eki.common", "eki.eve"})
+@SpringBootApplication(scanBasePackages = {"eki.common", "eki.eve"})
 public class EveAppApplication {
 
 	public static void main(String[] args) {
