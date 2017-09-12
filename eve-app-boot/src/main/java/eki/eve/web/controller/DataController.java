469220993fe46f3f3787773cae8ad1e0dd4c00dc
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-09-12 16:31:09 +0300

    Simple data api example.

---
+++ eve-app-boot/src/main/java/eki/eve/web/controller/DataController.java
@@ -0,0 +1,23 @@
+package eki.eve.web.controller;
+
+import eki.common.data.AppData;
+import eki.common.web.AppDataHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class DataController {
+
+	@Autowired
+	private AppDataHolder appDataHolder;
+
+	@RequestMapping(value="/data/app", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	public AppData getAppData(HttpServletRequest request) {
+		return appDataHolder.getAppData(request, "");
+	}
+
+}
