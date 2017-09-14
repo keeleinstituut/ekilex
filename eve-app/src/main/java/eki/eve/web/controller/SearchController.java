e0c0b77ece39280627d1b2697c14ff46431c5950
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-09-14 15:29:57 +0300

    Dummy details ajax request.

--- eve-app/src/main/java/eki/eve/web/controller/SearchController.java
+++ eve-app/src/main/java/eki/eve/web/controller/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 
@@ -24,6 +25,13 @@ public class SearchController {
 			model.addAttribute("searchFilter", searchFilter);
 		}
 		return "index";
+	}
+
+	@GetMapping("/details")
+	@ResponseBody
+	public String details(@RequestParam String id) {
+		logger.debug("doing details");
+		return "<div name=\"" + id + "_details\">Hunnikutes põnevaid detaile vormi <span style='font-weight: bold;'>" + id + "</span> kohta</div>";
 	}
 
 }
