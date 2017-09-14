ff412037e03c9e3a2554e52d337bd884eeb7faac
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-09-14 16:59:55 +0300

    Move javascript code from html to separate javascript file.

---
+++ eve-app/src/main/resources/view/js/eve-search.js
@@ -0,0 +1,12 @@
+// add on click handlers to details buttons in search result table
+function initialize() {
+    $('#results').find('[name="details"]').on('click', function (e) {
+        var id = $(e.target).data('id');
+        $.get('/details?id=' + id).done(function (data) {
+            $('[name="' + id + '_details"]').replaceWith(data);
+        }).fail(function (data) {
+            console.log(data);
+            alert('Detailide päring ebaõnnestus, proovige hiljem uuesti.');
+        });
+    })
+}
