a02f6a68d677e409d0c9889be1c8b3eb6f66bf4d
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-10-10 13:57:09 +0300

    Refactoring. Move termeki access logic into service.

--- ekilex-etl/src/main/java/eki/ekilex/runner/TermekiRunner.java
+++ ekilex-etl/src/main/java/eki/ekilex/runner/TermekiRunner.java
@@ -1,14 +1,13 @@
 package eki.ekilex.runner;
 
 import eki.common.service.db.BasicDbService;
-import eki.ekilex.service.TermekiDbService;
+import eki.ekilex.service.TermekiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static eki.common.constant.TableName.DATASET;
@@ -19,7 +18,7 @@ public class TermekiRunner {
 	private static Logger logger = LoggerFactory.getLogger(TermekiRunner.class);
 
 	@Autowired
-	private TermekiDbService termekiDbService;
+	private TermekiService termekiService;
 
 	@Autowired
 	private BasicDbService basicDbService;
@@ -32,7 +31,7 @@ public class TermekiRunner {
 	}
 
 	private boolean hasTermDatabaseAndIsKnownDataset(Integer baseId, String dataset) throws Exception {
-		return hasTermDatabase(baseId) && isKnownDataset(dataset);
+		return termekiService.hasTermDatabase(baseId) && isKnownDataset(dataset);
 	}
 
 	private boolean isKnownDataset(String dataset) throws Exception {
@@ -44,18 +43,5 @@ public class TermekiRunner {
 			logger.info("No datset with id {} defined in EKILEX", dataset);
 		}
 		return selectedDataset != null;
-	}
-
-	private boolean hasTermDatabase(Integer baseId) {
-
-		Map<String, Object> params = new HashMap<>();
-		params.put("baseId", baseId);
-		List<Map<String, Object>> result = termekiDbService.queryList("select * from termeki_termbases where termbase_id=:baseId", params);
-		if (!result.isEmpty()) {
-			logger.debug("Connection success, termeki base \"{}\".", result.get(0).get("termbase_name"));
-		} else {
-			logger.info("No termeki base with id found", baseId);
-		}
-		return !result.isEmpty();
 	}
 }
--- ekilex-etl/src/main/java/eki/ekilex/service/TermekiService.java
+++ ekilex-etl/src/main/java/eki/ekilex/service/TermekiService.java
@@ -1,16 +1,21 @@
 package eki.ekilex.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
-public class TermekiDbService {
+public class TermekiService {
+
+	private static Logger logger = LoggerFactory.getLogger(TermekiService.class);
 
 	@Autowired @Qualifier(value = "jdbcTemplateTermeki")
 	protected NamedParameterJdbcTemplate jdbcTemplate;
@@ -20,4 +25,18 @@ public class TermekiDbService {
 		List<Map<String, Object>> results = jdbcTemplate.queryForList(sqlScript, paramMap);
 		return results == null ? Collections.emptyList() : results;
 	}
+
+	public boolean hasTermDatabase(Integer baseId) {
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("baseId", baseId);
+		List<Map<String, Object>> result = queryList("select * from termeki_termbases where termbase_id=:baseId", params);
+		if (!result.isEmpty()) {
+			logger.debug("Connection success, termeki base \"{}\".", result.get(0).get("termbase_name"));
+		} else {
+			logger.info("No termeki base with id found", baseId);
+		}
+		return !result.isEmpty();
+	}
+
 }
