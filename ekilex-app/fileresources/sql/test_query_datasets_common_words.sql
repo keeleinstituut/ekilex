f8f4d0649455650bc59c630def4aa78965f9ef66
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-10-24 10:22:31 +0300

    Test data and test queries refactored to support dataset relations tables.

--- ekilex-app/fileresources/sql/test_query_datasets_common_words.sql
+++ ekilex-app/fileresources/sql/test_query_datasets_common_words.sql
@@ -2,18 +2,22 @@
 select w.id word_id,
        f.value word
 from word w,
-     paradigm p,
-     form f,
-     lexeme l1
+  paradigm p,
+  form f,
+  lexeme l1,
+  lexeme_dataset ld1
 where f.paradigm_id = p.id
-and   p.word_id = w.id
-and   f.is_word = true
-and   l1.word_id = w.id
-and   :dataset1 = all (l1.datasets)
-and   exists (select l2.word_id
-                  from lexeme l2
-                  where :dataset2 = all (l2.datasets)
-                  and   l1.word_id = l2.word_id)
+      and   p.word_id = w.id
+      and   f.is_word = true
+      and   l1.word_id = w.id
+      and   ld1.lexeme_id = l1.id
+      and   ld1.dataset_code = :dataset1
+      and   exists (select l2.word_id
+                    from lexeme l2,
+                      lexeme_dataset ld2
+                    where ld2.dataset_code = :dataset2
+                          and   l1.word_id = l2.word_id
+                          and   ld2.lexeme_id = l2.id)
 group by w.id,
-         f.value
+  f.value
 order by f.value;
--- ekilex-app/fileresources/sql/test_query_word_definitions.sql
+++ ekilex-app/fileresources/sql/test_query_word_definitions.sql
@@ -7,7 +7,7 @@ select w.word_id,
        l.id lexeme_id,
        m.id meaning_id,
        d.value definition,
-       l.datasets lexeme_datasets
+       ld.dataset_code lexeme_dataset
 from (select w.id word_id,
              f.value word,
              f.components,
@@ -21,6 +21,7 @@ from (select w.id word_id,
   inner join morph wm on w.morph_code = wm.code
   inner join morph_label wml on wml.code = wm.code and wml.lang = :labelLang and wml.type = :labelType
   inner join lexeme l on l.word_id = w.word_id
+  inner join lexeme_dataset ld on ld.lexeme_id = l.id
   inner join meaning m on m.id = l.meaning_id
   left outer join definition d on d.meaning_id = m.id
 where w.word like :wordPrefix
