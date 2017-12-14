2f0e24600b42aa3f63ab30bbf6fd12f4688e1224
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2017-12-14 14:51:37 +0200

    Fix error in lexeme freeform query.

--- ekilex-etl/src/main/resources/sql/select_lexeme_freeform_by_type_and_value.sql
+++ ekilex-etl/src/main/resources/sql/select_lexeme_freeform_by_type_and_value.sql
@@ -3,7 +3,7 @@ SELECT
 FROM
   LEXEME_FREEFORM lf, FREEFORM ff
 WHERE
-  lf.id = :lexeme_id
+  lf.lexeme_id = :lexeme_id
   AND lf.freeform_id = ff.id
   AND ff.type = :type
   AND ff.value_text = :value
