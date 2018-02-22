3406f332f14defb22878579de6f6b0053eb38db9
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2018-02-22 11:50:33 +0200

    Use different entity list in case of lex or term searches.

--- ekilex-app/src/main/java/eki/ekilex/constant/SearchEntity.java
+++ ekilex-app/src/main/java/eki/ekilex/constant/SearchEntity.java
@@ -1,5 +1,9 @@
 package eki.ekilex.constant;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public enum SearchEntity {
 	WORD(new SearchKey[] {SearchKey.VALUE, SearchKey.LANGUAGE}),
 	FORM(new SearchKey[] {SearchKey.VALUE, SearchKey.LANGUAGE}),
@@ -16,5 +20,13 @@ public enum SearchEntity {
 
 	public SearchKey[] getKeys() {
 		return keys;
+	}
+
+	public static List<SearchEntity> lexEntities() {
+		return asList(WORD, FORM, DEFINITION, USAGE);
+	}
+
+	public static List<SearchEntity> termEntities() {
+		return asList(WORD, FORM, DEFINITION, USAGE, CONCEPT_ID);
 	}
 }
