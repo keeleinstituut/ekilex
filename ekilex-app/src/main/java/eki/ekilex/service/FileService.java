c8cd5e14b0d94af90e6ffd2e579f51a1b091a0ee
Martin Laubre <martin.laubre@gmail.com>
2018-05-31 17:55:25 +0300

    installation manual. under construction

--- ekilex-app/src/main/java/eki/ekilex/service/FileService.java
+++ ekilex-app/src/main/java/eki/ekilex/service/FileService.java
@@ -1,21 +1,17 @@
 package eki.ekilex.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.PathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 
 @Service
 public class FileService {
