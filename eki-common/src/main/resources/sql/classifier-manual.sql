80d63d1bfa3c3649c81f0f02d3912c9572d7ce31
Martin Laubre <martin.laubre@gmail.com>
2018-07-12 14:33:44 +0300

    classifiers cleanup, fixes

--- eki-common/src/main/resources/sql/classifier-manual.sql
+++ eki-common/src/main/resources/sql/classifier-manual.sql
@@ -26,6 +26,7 @@ insert into label_type (code, value) values ('ekimorfo', 'ekimorfo');
 insert into label_type (code, value) values ('descrip', 'descrip');
 insert into label_type (code, value) values ('descrip2', 'descrip2');
 insert into label_type (code, value) values ('full', 'full');
+insert into label_type (code, value) values ('wordweb', 'wordweb');
 
 insert into dataset (code, name) values ('ss1', 'Eesti keele (üheköiteline) sõnaraamat (2010-2018/2019)');
 insert into dataset (code, name) values ('psv', 'Õppesõnastik A2/B1: eesti keele põhisõnavara (2014)');
