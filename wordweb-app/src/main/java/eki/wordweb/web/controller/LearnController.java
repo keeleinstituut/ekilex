0f402ea0bec516a26567e6cefc4191d0804f96d5
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2018-08-29 13:27:32 +0300

    Learn page and navigation logic for it.

--- wordweb-app/src/main/java/eki/wordweb/web/controller/LearnController.java
+++ wordweb-app/src/main/java/eki/wordweb/web/controller/LearnController.java
@@ -1,27 +1,24 @@
 package eki.wordweb.web.controller;
 
-import static java.util.Collections.emptyList;
-
+import eki.wordweb.constant.WebConstant;
+import eki.wordweb.data.WordsData;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import eki.wordweb.constant.WebConstant;
-import eki.wordweb.data.WordsData;
+import static java.util.Collections.emptyList;
 
 @ConditionalOnWebApplication
 @Controller
 @SessionAttributes(WebConstant.SESSION_BEAN)
-public class HomeController extends AbstractController {
+public class LearnController extends AbstractController {
 
-	@GetMapping(HOME_URI)
-	public String home(Model model) {
-
+	@GetMapping(LEARN_URI)
+	public String learn(Model model) {
 		populateModel("", new WordsData(emptyList(), emptyList()), model);
-
-		return HOME_PAGE;
+		return LEARN_PAGE;
 	}
 
 }
