b37158e0eea91384fd7d2354e6d70e75b65c93f6
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2018-12-21 13:15:04 +0200

    Honeypot protection against spam bot for registration form.

--- ekilex-app/src/main/java/eki/ekilex/web/controller/RegisterController.java
+++ ekilex-app/src/main/java/eki/ekilex/web/controller/RegisterController.java
@@ -4,6 +4,9 @@ import eki.ekilex.constant.WebConstant;
 import eki.ekilex.security.EkilexPasswordEncoder;
 import eki.ekilex.service.EmailService;
 import eki.ekilex.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +14,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 
+import static java.lang.Thread.sleep;
 import static java.util.Arrays.asList;
 
 @ConditionalOnWebApplication
 @Controller
 public class RegisterController implements WebConstant {
+
+	private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
 
 	private UserService userService;
 
@@ -40,7 +47,18 @@ public class RegisterController implements WebConstant {
 	public String registerNewUser(
 			@RequestParam("email") String email,
 			@RequestParam("name") String name,
-			Model model) {
+			@RequestParam(value = "email2", required = false) String honeyPot,
+			Model model,
+			HttpServletRequest request) {
+		if (StringUtils.isNotEmpty(honeyPot)) {
+			// bot protection triggered
+			logger.warn("Bot protection triggered : {} : {} : {} : {}", request.getRemoteAddr(), honeyPot, name);
+			try {
+				sleep(10 * 1000);
+			} catch (InterruptedException e) {
+			}
+			return "redirect:" + LOGIN_PAGE_URI;
+		}
 		if (userService.isValidNewUser(email, name)) {
 			String password = userService.generatePassword();
 			String encodedPassword = passwordEncoder.encode(password);
