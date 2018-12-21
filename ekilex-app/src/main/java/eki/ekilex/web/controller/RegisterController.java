1d16d4514bab8b96e08c45d7c3fe4c03b87146b9
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2018-12-21 11:55:36 +0200

    Generate simple password and save new user into database.

--- ekilex-app/src/main/java/eki/ekilex/web/controller/RegisterController.java
+++ ekilex-app/src/main/java/eki/ekilex/web/controller/RegisterController.java
@@ -1,6 +1,8 @@
 package eki.ekilex.web.controller;
 
 import eki.ekilex.constant.WebConstant;
+import eki.ekilex.security.EkilexPasswordEncoder;
+import eki.ekilex.service.UserService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RegisterController implements WebConstant {
 
+	private UserService userService;
+
+	private EkilexPasswordEncoder passwordEncoder;
+
+	public RegisterController(UserService userService, EkilexPasswordEncoder passwordEncoder) {
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
 	@GetMapping(REGISTER_PAGE_URI)
 	public String register() {
 		return REGISTER_PAGE;
@@ -22,7 +33,14 @@ public class RegisterController implements WebConstant {
 			@RequestParam("email") String email,
 			@RequestParam("name") String name,
 			Model model) {
-		model.addAttribute("register_message", "Under construction : " + email + " : " + name);
+		if (userService.isValidNewUser(email, name)) {
+			String password = userService.generatePassword();
+			String encodedPassword = passwordEncoder.encode(password);
+			userService.addNewUser(email, name, encodedPassword);
+			model.addAttribute("success_message", "Kasutaja registreeritud, parool on saadetud e-postile : " + email + " : parool -> " + password);
+		} else {
+			model.addAttribute("error_message", "Sellise nime või e-posti aadressiga kasutaja on juba registreeritud.");
+		}
 		return REGISTER_PAGE;
 	}
 
