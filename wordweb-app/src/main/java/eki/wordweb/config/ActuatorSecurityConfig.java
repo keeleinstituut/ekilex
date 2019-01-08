8eed6af5f352fd714ee5f894ed2d4500bbab7353
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2019-01-08 13:23:51 +0200

    Disable csrf, if enabled else post request do not work on unauthorized pages.

--- wordweb-app/src/main/java/eki/wordweb/config/ActuatorSecurityConfig.java
+++ wordweb-app/src/main/java/eki/wordweb/config/ActuatorSecurityConfig.java
@@ -19,6 +19,9 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
 			.requestMatchers(EndpointRequest.to(ShutdownEndpoint.class))
 			.hasRole("ACTUATOR_ADMIN")
 			.and()
-			.httpBasic();
+			.httpBasic()
+			.and()
+			.csrf().disable();
 	}
+
 }
