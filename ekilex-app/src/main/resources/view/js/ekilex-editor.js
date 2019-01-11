21ee72f598f9ae2a6878419ece6b1e18e5adf578
Raigo Ukkivi <raigo.ukkivi@gmail.com>
2019-01-11 17:05:01 +0200

    Ekilex editor refactored to use contenteditable html element, simple WYSIWYG editor.

--- ekilex-app/src/main/resources/view/js/ekilex-editor.js
+++ ekilex-app/src/main/resources/view/js/ekilex-editor.js
@@ -1,72 +1,68 @@
 // Javascript methods for Ekilex custom editor
 
+function openEkiEditorDlg(elem) {
+    let targetName = $(elem).data('target-elem');
+    let targetElement = $('[name="' + targetName + '"]');
+    let editDlg = $($(elem).data('target'));
+    let modifyFld = editDlg.find('[name=editFld]');
+    modifyFld.html(targetElement.data('value') === undefined ? targetElement.text() : targetElement.data('value'));
+    editDlg.find('[name=id]').val(targetElement.data('id'));
+    editDlg.find('[name=opCode]').val(targetElement.data('op-code'));
+    editDlg.find('button[type="submit"]').off('click').on('click', function(e) {
+        editDlg.find('[name=value]').val(modifyFld.html());
+        submitDialog(e, editDlg, 'Andmete muutmine ebaõnnestus.')
+    });
+    initEkiEditor(editDlg);
+}
+
 function initEkiEditor(editDlg) {
-    let editorElem = editDlg.find('[name=value]');
-    let previewElement = editDlg.find('[name=preview]');
+    let editorElem = editDlg.find('[name=editFld]');
     let menuElement = editDlg.find('.eki-editor-menu');
     editorElem.off('keydown').on('keydown', function (e) {
         let isEditorCommand = e.ctrlKey === true && (e.key === 'm' || e.key === 'n');
         if (isEditorCommand) {
-            let isTextSelected = e.target.selectionStart !== e.target.selectionEnd;
-            if (isTextSelected) {
-                if (e.key === 'm') {
-                    menuElement.addClass('show');
-                    let selectElem = menuElement.find('select');
-                    selectElem.val(selectElem.find('option').first().val());
-                    selectElem.focus();
-                    e.preventDefault();
-                    e.stopPropagation();
-                } else if (e.key === 'n') {
-                    let preSelect = e.target.value.substring(0, e.target.selectionStart);
-                    let selectedText = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
-                    let postSelect = e.target.value.substring(e.target.selectionEnd);
-                    selectedText = selectedText.replace(/<\/?eki-elem-(.*?)>/g, '');
-                    e.target.value = preSelect + selectedText + postSelect;
-                    editorElem.trigger('input');
-                }
-                console.log(e);
+            if (e.key === 'm') {
+                menuElement.addClass('show');
+                let selectElem = menuElement.find('select');
+                selectElem.val(selectElem.find('option').first().val());
+                selectElem.focus();
+            } else if (e.key === 'n') {
+                let cleanedHtml = editorElem.html().replace(/<\/?eki-elem-(.*?)>/g, '');
+                editorElem.html(cleanedHtml);
             }
+            e.preventDefault();
+            e.stopPropagation();
         }
     });
     menuElement.off('keydown').on('keydown', function (e) {
         if ((e.key === 'Escape' || e.key === 'Enter') && menuElement.hasClass('show')) {
-            menuElement.removeClass('show');
             e.preventDefault();
             e.stopPropagation();
+            menuElement.removeClass('show');
             editorElem.focus();
             if (e.key === 'Enter') {
                 let ekiTag = menuElement.find('option:selected').val();
-                addTag(editorElem, '<' + ekiTag + '>', '</' + ekiTag + '>');
+                addNode(ekiTag);
             }
         }
     });
     menuElement.off('dblclick').on('dblclick', function (e) {
         if (menuElement.hasClass('show')) {
-            menuElement.removeClass('show');
             e.preventDefault();
             e.stopPropagation();
+            menuElement.removeClass('show');
             editorElem.focus();
             let ekiTag = menuElement.find('option:selected').val();
-            addTag(editorElem, '<' + ekiTag + '>', '</' + ekiTag + '>');
+            addNode(ekiTag);
         }
     });
-    if (previewElement !== undefined) {
-        previewElement.html(editorElem.val());
-        editorElem.off('input').on('input', function() {
-            previewElement.html(editorElem.val());
-        });
-    }
 }
 
-function addTag(editorElement, startTag, endTag) {
-    let selectionStart = editorElement.prop('selectionStart');
-    let selectionEnd = editorElement.prop('selectionEnd');
-    let isTextSelected = selectionStart !== selectionEnd;
-    if (isTextSelected) {
-        let preSelect = editorElement.val().substring(0, selectionStart);
-        let selectedText = editorElement.val().substring(selectionStart, selectionEnd);
-        let postSelect = editorElement.val().substring(selectionEnd);
-        editorElement.val(preSelect + startTag + selectedText + endTag + postSelect);
-        editorElement.trigger('input');
+function addNode(ekiTag) {
+    let sel = window.getSelection();
+    if (sel.rangeCount) {
+        let selectedRange = sel.getRangeAt(0);
+        let ekiNode = document.createElement(ekiTag);
+        selectedRange.surroundContents(ekiNode);
     }
 }
