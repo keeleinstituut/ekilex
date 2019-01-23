fd0e4a58f561d7fcb67fc1fe133914b62955fc8a
Martin Laubre <martin.laubre@gmail.com>
2019-01-23 19:26:31 +0200

    forgot to clean up testbench

--- ekilex-etl/src/main/java/eki/ekilex/sandbox/TestBench.java
+++ ekilex-etl/src/main/java/eki/ekilex/sandbox/TestBench.java
@@ -1,17 +1,10 @@
 package eki.ekilex.sandbox;
 
-import org.apache.commons.lang3.RegExUtils;
-
 // try out anything
 public class TestBench {
 
 	public static void main(String[] args) throws Exception {
 
-		String value = "aaa <eki-elem-1>bbb</eki-elem-1> ccc <eki-elem-2>ddd</eki-elem-2> eee";
-
-		String clean = RegExUtils.removePattern(value, "<[^>]*>");
-
-		System.out.println(clean);
 	}
 
 }
