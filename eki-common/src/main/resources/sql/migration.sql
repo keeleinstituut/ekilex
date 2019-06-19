871348c824fa8826f6d8385c11c5a31b4cba2ffc
Kaido Laine <kaido.laine@gmail.com>
2019-06-19 13:53:33 +0300

    word_relation.relation_status, word_relation_param table script

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -2,3 +2,19 @@ alter table dataset add column is_visible boolean default true;
 update dataset set is_visible = false where code = 'mab';
 
 alter table eki_user add column recovery_key varchar(60);
+
+-- 19.6.19
+create table word_relation_param
+(
+  id bigserial primary key,
+  word_relation_id bigint references word_relation(id) on delete cascade not null,
+  param_name text not null,
+  value_float float,
+  value_int int,
+  value_boolean boolean
+);
+alter sequence word_relation_param_id_seq restart with 10000;
+
+create index word_relation_param_word_relation_id_idx on word_relation_param(word_relation_id);
+
+alter table word_relation add relation_status varchar(100);
