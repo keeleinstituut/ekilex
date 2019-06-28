c6027b5c89b3d49d3dcf75cbda34b9ed01246594
Kaido Laine <kaido.laine@gmail.com>
2019-06-28 12:09:16 +0300

    Remove obsolete unit test code

--- ekilex-app/src/test/java/eki/ekilex/test/SynSearchServiceTest.java
+++ ekilex-app/src/test/java/eki/ekilex/test/SynSearchServiceTest.java
@@ -18,11 +18,7 @@ import eki.common.test.TestEnvInitialiser;
 import eki.ekilex.data.SynRelation;
 import eki.ekilex.data.SynRelationParamTuple;
 import eki.ekilex.service.SynSearchDbService;
-import eki.ekilex.service.SynSearchService;
-import eki.ekilex.service.UserService;
 import eki.ekilex.service.util.ConversionUtil;
-
-//import static com.google.common.truth.Truth.;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -37,54 +33,18 @@ public class SynSearchServiceTest extends AbstractTest {
 	private SynSearchDbService synSearchDbService;
 
 	@Autowired
-	private SynSearchService synSearchService;
-
-	@Autowired
 	private ConversionUtil conversionUtil;
-
-	@Autowired
-	private UserService userService;
 
 	@Before
 	public void beforeTest() throws Exception {
 		testEnvInitialiser.initDatabase();
-		initSecurity();
 	}
 
-	// @Test
-	// public void testGetExistingOppositeRelation() {
-	// 	Relation relation = synSearchDbService.getOppositeRelation(1003L, 1006L, "raw", "est", "descrip");
-	// 	assertThat(relation).isNotNull();
-	// 	assertThat(relation.getWord()).isEqualTo("hallakord");
-	// }
-	//
-	// @Test
-	// public void testGetNotExistingOppositeRelation() {
-	// 	Relation relation = synSearchDbService.getOppositeRelation(1003L, 1004L, "raw", "est", "descrip");
-	// 	assertThat(relation).isNull();
-	// }
-	//
-	// @Test
-	// public void testWordRelationParams() {
-	// 	List<RelationParam> relationParameters = synSearchDbService.getRelationParameters(1001L);
-	// 	assertThat(relationParameters.size()).isEqualTo(2);
-	// 	assertThat(relationParameters.get(1).getValue()).isEqualTo("0.003");
-	// }
-	//
-	// @Test
-	// public void testSynRelationPopulation() {
-	//
-	// 	WordDetails details = synSearchService.getWordDetailsSynonyms(1003L, new ArrayList<>());
-	//
-	// 	assertThat(details).isNotNull();
-	// }
-	//
 	@Test
 	public void testGetSynRelationsTuples() {
 		List<SynRelationParamTuple> paramTuples = synSearchDbService.getWordSynRelations(1003L, "raw", "est", "descrip");
 		List<SynRelation> relations = conversionUtil.composeSynRelations(paramTuples);
 
-		assertThat(paramTuples).isNotNull();
-		assertThat(relations).isNotNull();
+		assertThat(relations.size()).isEqualTo(2);
 	}
 }
