f878579ff11ae13b7f53537b4f8e35450d36dae0
Kaido Laine <kaido.laine@gmail.com>
2019-08-07 14:50:46 +0300

    Show iso2 language codes in join and word select pages

--- ekilex-app/src/main/java/eki/ekilex/web/controller/LexEditController.java
+++ ekilex-app/src/main/java/eki/ekilex/web/controller/LexEditController.java
@@ -21,8 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import eki.ekilex.constant.WebConstant;
 import eki.ekilex.data.WordLexeme;
 import eki.ekilex.service.CommonDataService;
@@ -30,6 +28,7 @@ import eki.ekilex.service.CompositionService;
 import eki.ekilex.service.LexSearchService;
 import eki.ekilex.web.bean.SessionBean;
 import eki.ekilex.web.util.SearchHelper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ConditionalOnWebApplication
 @Controller
@@ -154,5 +153,10 @@ public class LexEditController implements WebConstant {
 
 		ObjectMapper jsonMapper = new ObjectMapper();
 		return jsonMapper.writeValueAsString(response);
+	}
+
+	@ModelAttribute("iso2languages")
+	public Map<String, String> getIso2Languages() {
+		return commonDataService.getLanguagesIso2Map();
 	}
 }
