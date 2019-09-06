e50c2752a2d18f344127dcd78fbfb077f6bfdd68
Kaido Laine <kaido.laine@gmail.com>
2019-09-06 09:30:12 +0300

    Some additions to migration.sql considering further db changes

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -3,4 +3,8 @@ insert into eki_user_profile (user_id) (select eki_user.id from eki_user);
 
 -- 02.09.2019
 alter table lexeme add column type varchar(50);
-update lexeme set type = 'PRIMARY';
+
+-- this can be executed after the type introducing implementation is finished
+update lexeme set type = 'PRIMARY' where type is null;
+
+-- TODO change lexeme.type not nullable
