8927bce520bcd6bd25c1e0c8de03621e38de451a
Martin Laubre <martin.laubre@gmail.com>
2019-09-13 12:17:36 +0300

    missing class import declaration

--- eki-common/src/main/java/eki/common/service/db/BasicDbService.java
+++ eki-common/src/main/java/eki/common/service/db/BasicDbService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.dao.EmptyResultDataAccessException;
