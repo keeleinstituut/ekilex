92ec8bcaf513b4b01cc5a3d51f336a66a45c534e
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2019-11-05 15:57:11 +0200

    word relation mapping updated

--- eki-common/src/main/resources/sql/delivery.sql
+++ eki-common/src/main/resources/sql/delivery.sql
@@ -1,1 +1,3 @@
 -- upgrade from ver 1.9.0 to 1.10.0
+
+insert into word_rel_mapping (code1, code2) values ('head', 'ühend');
--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -2,3 +2,6 @@
 create index lifecycle_log_event_by_lower_idx on lifecycle_log(lower(event_by));
 create index lifecycle_log_event_on_ms_idx on lifecycle_log((date_part('epoch', event_on) * 1000));
 create index freeform_complexity_idx on freeform(complexity);
+
+-- 05.11.2019
+insert into word_rel_mapping (code1, code2) values ('head', 'ühend');
