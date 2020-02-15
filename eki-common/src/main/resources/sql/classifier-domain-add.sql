5728ce7a33ac2beac68840a5631c2719bc2a6adb
Kaur Manniko <kaur2k@gmail.com>
2020-02-15 20:20:29 +0200

    added domains in sepparate file

---
+++ eki-common/src/main/resources/sql/classifier-domain-add.sql
@@ -0,0 +1,15 @@
+insert into domain (origin, code, parent_origin, parent_code, datasets) values ('kto', 'tikkimine', 'kto', 'tekstiilikäsitöö', '{}');
+insert into domain (origin, code, parent_origin, parent_code, datasets) values ('kto', 'ketramine', 'kto', 'tekstiilikäsitöö', '{}');
+insert into domain (origin, code, parent_origin, parent_code, datasets) values ('kto', 'aasnõelumine', 'kto', 'tekstiilikäsitöö', '{}');
+insert into domain (origin, code, parent_origin, parent_code, datasets) values ('kto', 'võrkpõime', 'kto', 'tekstiilikäsitöö', '{}');
+
+insert into domain (origin, code, datasets) values ('tet', 'lavastusliik', '{}');
+
+insert into domain_label (origin, code, value, lang, type) values ('kto', 'tikkimine', 'tikkimine', 'est', 'descrip');
+insert into domain_label (origin, code, value, lang, type) values ('kto', 'ketramine', 'ketramine', 'est', 'descrip');
+insert into domain_label (origin, code, value, lang, type) values ('kto', 'aasnõelumine', 'aasnõelumine', 'est', 'descrip');
+insert into domain_label (origin, code, value, lang, type) values ('kto', 'võrkpõime', 'võrkpõime', 'est', 'descrip');
+
+insert into domain_label (origin, code, value, lang, type) values ('tet', 'lavastusliik', 'lavastusliik', 'est', 'descrip');
+
+
