5686099cbd117393ab8fd8a45df702a8f29b4cd0
Kaur Manniko <kaur2k@gmail.com>
2020-02-19 02:25:43 +0200

    eliminate run only once statements

--- eki-common/src/main/resources/sql/delivery.sql
+++ eki-common/src/main/resources/sql/delivery.sql
@@ -1,11 +1,11 @@
 -- upgrade from ver 1.12.0 to 1.13.0
 
 -- ekilexis ühekordselt:
-create extension unaccent;
+-- create extension unaccent;
 
 -- wordwebis ühekordselt:
-create extension pg_trgm;
-create extension fuzzystrmatch;
+-- create extension pg_trgm;
+-- create extension fuzzystrmatch;
 
 alter table eki_user add column terms_ver varchar(100) null;
 alter table eki_user_profile
