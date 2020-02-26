3a57b73f6cd51a1173193dca846232b2445b79b0
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2020-02-26 18:10:54 +0200

    refactored lexeme collocations cloning code

---
+++ ekilex-app/src/main/java/eki/ekilex/data/LexCollocationTuple.java
@@ -0,0 +1,150 @@
+package eki.ekilex.data;
+
+import java.util.List;
+
+import eki.common.data.AbstractDataObject;
+
+public class LexCollocationTuple extends AbstractDataObject {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long lexCollocId;
+
+	private Long relGroupId;
+
+	private String memberForm;
+
+	private String conjunct;
+
+	private Float weight;
+
+	private Integer memberOrder;
+
+	private Integer groupOrder;
+
+	private Long collocId;
+
+	private String collocValue;
+
+	private String collocDefinition;
+
+	private Float collocFrequency;
+
+	private Float collocScore;
+
+	private List<String> collocUsages;
+
+	private String collocComplexity;
+
+	public Long getLexCollocId() {
+		return lexCollocId;
+	}
+
+	public void setLexCollocId(Long lexCollocId) {
+		this.lexCollocId = lexCollocId;
+	}
+
+	public Long getRelGroupId() {
+		return relGroupId;
+	}
+
+	public void setRelGroupId(Long relGroupId) {
+		this.relGroupId = relGroupId;
+	}
+
+	public String getMemberForm() {
+		return memberForm;
+	}
+
+	public void setMemberForm(String memberForm) {
+		this.memberForm = memberForm;
+	}
+
+	public String getConjunct() {
+		return conjunct;
+	}
+
+	public void setConjunct(String conjunct) {
+		this.conjunct = conjunct;
+	}
+
+	public Float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Float weight) {
+		this.weight = weight;
+	}
+
+	public Integer getMemberOrder() {
+		return memberOrder;
+	}
+
+	public void setMemberOrder(Integer memberOrder) {
+		this.memberOrder = memberOrder;
+	}
+
+	public Integer getGroupOrder() {
+		return groupOrder;
+	}
+
+	public void setGroupOrder(Integer groupOrder) {
+		this.groupOrder = groupOrder;
+	}
+
+	public Long getCollocId() {
+		return collocId;
+	}
+
+	public void setCollocId(Long collocId) {
+		this.collocId = collocId;
+	}
+
+	public String getCollocValue() {
+		return collocValue;
+	}
+
+	public void setCollocValue(String collocValue) {
+		this.collocValue = collocValue;
+	}
+
+	public String getCollocDefinition() {
+		return collocDefinition;
+	}
+
+	public void setCollocDefinition(String collocDefinition) {
+		this.collocDefinition = collocDefinition;
+	}
+
+	public Float getCollocFrequency() {
+		return collocFrequency;
+	}
+
+	public void setCollocFrequency(Float collocFrequency) {
+		this.collocFrequency = collocFrequency;
+	}
+
+	public Float getCollocScore() {
+		return collocScore;
+	}
+
+	public void setCollocScore(Float collocScore) {
+		this.collocScore = collocScore;
+	}
+
+	public List<String> getCollocUsages() {
+		return collocUsages;
+	}
+
+	public void setCollocUsages(List<String> collocUsages) {
+		this.collocUsages = collocUsages;
+	}
+
+	public String getCollocComplexity() {
+		return collocComplexity;
+	}
+
+	public void setCollocComplexity(String collocComplexity) {
+		this.collocComplexity = collocComplexity;
+	}
+}
