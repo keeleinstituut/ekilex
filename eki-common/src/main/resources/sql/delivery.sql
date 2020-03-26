a5dc963a33a930ed3366674b7a9248df2e19bbe9
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2020-03-26 16:57:22 +0200

    delete definitions that are from 'kol'

--- eki-common/src/main/resources/sql/delivery.sql
+++ eki-common/src/main/resources/sql/delivery.sql
@@ -1,7 +1,29 @@
--- kustuta qqst pärit seletused
+-- upgrade from ver 1.15.0 to 1.16.0
+-- kui koli seletus on tähenduse ainuke sss seletus, siis muudab selle detailseks
+update definition d
+set complexity = 'DETAIL'
+where d.complexity = 'DEFAULT'
+  and exists(select dd.definition_id
+             from definition_dataset dd
+             where dd.definition_id = d.id
+               and dd.dataset_code = 'sss')
+  and not exists(select dd.definition_id
+                 from definition_dataset dd
+                 where dd.definition_id = d.id
+                   and dd.dataset_code != 'sss')
+  and not exists(select d2.id
+                 from definition d2
+                 where d2.meaning_id = d.meaning_id
+                   and d2.id != d.id
+                   and exists(select dd.definition_id
+                              from definition_dataset dd
+                              where dd.definition_id = d2.id
+                                and dd.dataset_code = 'sss'));
+
+-- kustutab koli seletused
 delete
 from definition d
-where d.complexity = 'SIMPLE2'
+where complexity = 'DEFAULT'
   and exists(select dd.definition_id
              from definition_dataset dd
              where dd.definition_id = d.id
--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -1,2 +1,36 @@
 create index collocation_value_idx on collocation(value);
 analyze collocation;
+
+-- kui koli seletus on tähenduse ainuke sss seletus, siis muudab selle detailseks
+update definition d
+set complexity = 'DETAIL'
+where d.complexity = 'DEFAULT'
+  and exists(select dd.definition_id
+             from definition_dataset dd
+             where dd.definition_id = d.id
+               and dd.dataset_code = 'sss')
+  and not exists(select dd.definition_id
+                 from definition_dataset dd
+                 where dd.definition_id = d.id
+                   and dd.dataset_code != 'sss')
+  and not exists(select d2.id
+                 from definition d2
+                 where d2.meaning_id = d.meaning_id
+                   and d2.id != d.id
+                   and exists(select dd.definition_id
+                              from definition_dataset dd
+                              where dd.definition_id = d2.id
+                                and dd.dataset_code = 'sss'));
+
+-- kustutab koli seletused
+delete
+from definition d
+where complexity = 'DEFAULT'
+  and exists(select dd.definition_id
+             from definition_dataset dd
+             where dd.definition_id = d.id
+               and dd.dataset_code = 'sss')
+  and not exists(select dd.definition_id
+                 from definition_dataset dd
+                 where dd.definition_id = d.id
+                   and dd.dataset_code != 'sss');
