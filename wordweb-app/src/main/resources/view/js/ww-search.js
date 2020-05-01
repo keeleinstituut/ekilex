a7164b05b0c058ece1ad2d42bd00fd4c5844dfc7
Hardi Niilo <hardi.niilo@twn.ee>
2020-05-01 12:22:13 +0300

    FIX – morphology styles and collapse behaviour

--- wordweb-app/src/main/resources/view/js/ww-search.js
+++ wordweb-app/src/main/resources/view/js/ww-search.js
@@ -1,136 +1,153 @@
 var windowWidthTreshold = 768;
 
 function fetchDetails(wordId, word, lang, wordSelectUrl) {
-	var detailsDiv = $('.word-details');
-	var wordDetailsUrlWithParams = wordDetailsUrl + "/" + wordId;
-	$.get(wordDetailsUrlWithParams).done(function(data) {
-		detailsDiv.replaceWith(data);
-		if (word.indexOf('/') !== -1) {
-			wordSelectUrl = wordSelectUrl.replace(word, encodeURIComponent(word));
-		}
-		var historyState = {
-			wordId : wordId,
-			word : word,
-			wordSelectUrl : wordSelectUrl
-		};
-		history.pushState(historyState, "Sõnaveeb", wordSelectUrl);
-		fetchCorpSentences(lang, word);
-		setHomonymNrVisibility();
-		$('.word-details [data-toggle="tooltip"]').tooltip();
-		$('[data-toggle="popover"]').popover({
-			placement: 'top',
-			trigger: 'focus'
-		});
-		var lightbox = new SimpleLightbox(
-			'.gallery-image',
-			{
-				history: false,
-				captionPosition: 'outside',
-				navText:['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
-				closeText: '<i class="fas fa-times"></i>'
-			});
-	}).fail(function(data) {
-		alert(messages.search_failure);
-	})
+    var detailsDiv = $('.word-details');
+    var wordDetailsUrlWithParams = wordDetailsUrl + "/" + wordId;
+    $.get(wordDetailsUrlWithParams).done(function (data) {
+        detailsDiv.replaceWith(data);
+        if (word.indexOf('/') !== -1) {
+            wordSelectUrl = wordSelectUrl.replace(word, encodeURIComponent(word));
+        }
+        var historyState = {
+            wordId: wordId,
+            word: word,
+            wordSelectUrl: wordSelectUrl
+        };
+        history.pushState(historyState, "Sõnaveeb", wordSelectUrl);
+        fetchCorpSentences(lang, word);
+        setHomonymNrVisibility();
+        $('.word-details [data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover({
+            placement: 'top',
+            trigger: 'focus'
+        });
+        var lightbox = new SimpleLightbox(
+            '.gallery-image',
+            {
+                history: false,
+                captionPosition: 'outside',
+                navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+                closeText: '<i class="fas fa-times"></i>'
+            });
+        activateCollapseBtn();
+    }).fail(function (data) {
+        alert(messages.search_failure);
+    })
 }
 
 function setHomonymNrVisibility() {
-	var nrOfHomonyms = $(".homonym-item").length;
-	if (nrOfHomonyms === 1) {
-		$('.word-details-homonym-nr').addClass('d-none');
-	}
+    var nrOfHomonyms = $(".homonym-item").length;
+    if (nrOfHomonyms === 1) {
+        $('.word-details-homonym-nr').addClass('d-none');
+    }
 }
 
 function fetchCorpSentences(lang, word) {
-	var corpDiv = $("#korp");
-	var corpSentencesUrl = applicationUrl + 'korp/' + lang + '/' + encodeURIComponent(word);
-	$.get(corpSentencesUrl).done(function(data) {
-		corpDiv.replaceWith(data);
-	}).fail(function(data) {
-	})
+    var corpDiv = $("#korp");
+    var corpSentencesUrl = applicationUrl + 'korp/' + lang + '/' + encodeURIComponent(word);
+    $.get(corpSentencesUrl).done(function (data) {
+        corpDiv.replaceWith(data);
+    }).fail(function (data) {
+    })
 }
 
-$(function() {
-	$('[data-toggle="tooltip"]').tooltip({
-		container : 'body'
-	});
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        container: 'body'
+    });
 });
 
 $(document).on("click", ".menu-btn", function () {
-	if ($(this).attr('aria-expanded') === 'false') {
-		$(this).attr('aria-expanded', 'false');
-	} else {
-		$(this).attr('aria-expanded', 'true');
-	}
+    if ($(this).attr('aria-expanded') === 'false') {
+        $(this).attr('aria-expanded', 'false');
+    } else {
+        $(this).attr('aria-expanded', 'true');
+    }
+});
+function activateCollapseBtn(){
+    $(".btn-collapse").on("click", function () {
+        $(this).toggleClass('show');
+
+        if ($(this).data("see-less") && $(this).data("see-more")) {
+            console.log("click");
+            let _txt;
+            if ($(this).hasClass('show')) {
+                _txt = $(this).data("see-less");
+            } else {
+                _txt = $(this).data("see-more");
+            }
+
+            $(this).find('.btn-txt').text(_txt);
+        }
+    });
+}
+
+
+$(document).on("click", ".show-more", function () {
+
+    // button behaviour
+    let buttonText = '';
+    if ($(this).attr('aria-expanded') === 'false') {
+        $(this).attr('aria-expanded', 'true');
+        $(this).find("i").removeClass("fa-angle-down").addClass("fa-angle-up");
+        buttonText = $(this).data("see-less");
+    } else {
+        $(this).attr('aria-expanded', 'false');
+        $(this).find("i").removeClass("fa-angle-up").addClass("fa-angle-down");
+        buttonText = $(this).data("see-more");
+    }
+    $(this).find("span").text(buttonText);
+
+    if ($(this).data("target") !== undefined) {
+        $($(this).data("target")).toggleClass('collapse');
+    } else {
+        // TODO: Refactor all of this selector mess
+        $(this).parents(".word-relations, .dependencies, .collocations-section, .corp-panel")
+            .toggleClass("expand");
+
+        $(this).parents(".word-relations, .meaning-panel, .dependencies, .collocations-section, .corp-panel")
+            .find(".colloc-col, .label, .label-md, .corp-panel div:nth-child(n+5), .colloc-heading, .colloc-name, .secondary-morph, .word-options, .sentence-wrapper")
+            .toggleClass("fade-target");
+
+        $(this).parents(".word-relations, .meaning-panel, .dependencies, .collocations-section, .corp-panel")
+            .find(".colloc-fulldata .colloc-col:lt(3), .dependencies .full-group .word-options:lt(10), .sentence-wrapper:lt(2)")
+            .removeClass("fade-target");
+    }
 });
 
-$(document).on("click", ".show-more", function() {
-
-	// button behaviour
-	let buttonText = '';
-	if ($(this).attr('aria-expanded') === 'false') {
-		$(this).attr('aria-expanded', 'true');
-		$(this).find("i").removeClass("fa-angle-down").addClass("fa-angle-up");
-		buttonText = $(this).data("see-less");
-	} else {
-		$(this).attr('aria-expanded', 'false');
-		$(this).find("i").removeClass("fa-angle-up").addClass("fa-angle-down");
-		buttonText = $(this).data("see-more");
-	}
-	$(this).find("span").text(buttonText);
-
-	if($(this).data("target") !== undefined){
-		console.log($(this).data("target"));
-		$($(this).data("target")).toggleClass('collapse');
-	}
-	else{
-		// TODO: Refactor all of this selector mess
-		$(this).parents(".word-relations, .dependencies, .collocations-section, .corp-panel")
-				.toggleClass("expand");
-
-		$(this).parents(".word-relations, .meaning-panel, .dependencies, .collocations-section, .corp-panel")
-				.find(".colloc-col, .label, .label-md, .corp-panel div:nth-child(n+5), .colloc-heading, .colloc-name, .secondary-morph, .word-options, .sentence-wrapper")
-				.toggleClass("fade-target");
-
-		$(this).parents(".word-relations, .meaning-panel, .dependencies, .collocations-section, .corp-panel")
-				.find(".colloc-fulldata .colloc-col:lt(3), .dependencies .full-group .word-options:lt(10), .sentence-wrapper:lt(2)")
-				.removeClass("fade-target");
-	}
+$(window).on("popstate", function (e) {
+    e.preventDefault();
+    var historyState = e.originalEvent.state;
+    if (historyState != null) {
+        window.location = historyState.wordSelectUrl;
+    }
 });
 
-$(window).on("popstate", function(e) {
-	e.preventDefault();
-	var historyState = e.originalEvent.state;
-	if (historyState != null) {
-		window.location = historyState.wordSelectUrl;
-	}
+$(document).on("click", "a[id^='word-details-link']", function () {
+    var wordWrapperForm = $(this).closest("form");
+    var wordId = wordWrapperForm.children("[name='word-id']").val();
+    var word = wordWrapperForm.children("[name='word-value']").val();
+    var lang = wordWrapperForm.children("[name='word-lang']").val();
+    var wordSelectUrl = wordWrapperForm.children("[name='word-select-url']").val();
+    fetchDetails(wordId, word, lang, wordSelectUrl);
 });
 
-$(document).on("click", "a[id^='word-details-link']", function() {
-	var wordWrapperForm = $(this).closest("form");
-	var wordId = wordWrapperForm.children("[name='word-id']").val();
-	var word = wordWrapperForm.children("[name='word-value']").val();
-	var lang = wordWrapperForm.children("[name='word-lang']").val();
-	var wordSelectUrl = wordWrapperForm.children("[name='word-select-url']").val();
-	fetchDetails(wordId, word, lang, wordSelectUrl);
+$(document).on("click", ".homonym-item", function () {
+    $(".homonym-list-item").removeClass("selected last-selected");
+    $(".homonym-item:first").removeClass("animation-target").dequeue();
+    $(this).parents(".homonym-list-item").addClass("selected last-selected");
+    var homonymList = $('.homonym-list');
+    if ($(window).width() >= windowWidthTreshold) {
+        homonymList.animate({
+            scrollLeft: $('.homonym-list-item.selected .homonym-item-wrap').parent().position().left - $('.search-panel').offset().left + homonymList.scrollLeft()
+        }, 200);
+    }
+    setSelectedHomonymValueForMobile($(this).html());
+    $(".homonym-list").removeClass("expand");
 });
 
-$(document).on("click", ".homonym-item", function() {
-	$(".homonym-list-item").removeClass("selected last-selected");
-	$(".homonym-item:first").removeClass("animation-target").dequeue();
-	$(this).parents(".homonym-list-item").addClass("selected last-selected");
-	var homonymList = $('.homonym-list');
-	if ($(window).width() >= windowWidthTreshold) {
-		homonymList.animate({
-			scrollLeft : $('.homonym-list-item.selected .homonym-item-wrap').parent().position().left - $('.search-panel').offset().left + homonymList.scrollLeft()
-		}, 200);
-	}
-	setSelectedHomonymValueForMobile($(this).html());
-	$(".homonym-list").removeClass("expand");
-});
-
-$(document).on("click", "[name='word-form-btn']", function() {
-	var word = $(this).data('word');
-	$("input[name = 'searchWord']").val(word);
-	$('#search-btn').trigger('click');
+$(document).on("click", "[name='word-form-btn']", function () {
+    var word = $(this).data('word');
+    $("input[name = 'searchWord']").val(word);
+    $('#search-btn').trigger('click');
 });
