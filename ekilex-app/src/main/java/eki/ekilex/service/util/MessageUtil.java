b7f4099a14e3023cc72c074fb4e4204db467bc24
Martin Laubre <martin.laubre@gmail.com>
2020-06-24 19:55:16 +0300

    yet another attempt for globally acceptable way of reading text file in classpath

--- ekilex-app/src/main/java/eki/ekilex/service/util/MessageUtil.java
+++ ekilex-app/src/main/java/eki/ekilex/service/util/MessageUtil.java
@@ -1,14 +1,15 @@
 package eki.ekilex.service.util;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 import eki.common.constant.GlobalConstant;
 
@@ -17,14 +18,17 @@ public class MessageUtil implements InitializingBean, GlobalConstant {
 
 	private static final String MOTIVATIONAL_TXT_FILE_PATH = "messages/motivational.txt";
 
+	@Autowired
+	private ResourceLoader resourceLoader;
+
 	private List<String> positiveQuotes;
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		File txtFile = ResourceUtils.getFile("classpath:" + MOTIVATIONAL_TXT_FILE_PATH);
-		FileInputStream txtFileStream = new FileInputStream(txtFile);
-		positiveQuotes = IOUtils.readLines(txtFileStream, UTF_8);
-		txtFileStream.close();
+		Resource txtFileResource = resourceLoader.getResource("classpath:" + MOTIVATIONAL_TXT_FILE_PATH);
+		InputStream txtFileResInputStream = txtFileResource.getInputStream();
+		positiveQuotes = IOUtils.readLines(txtFileResInputStream, UTF_8);
+		txtFileResInputStream.close();
 	}
 
 	public synchronized String getPositiveQuote() {
