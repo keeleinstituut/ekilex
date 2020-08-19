93aa06ef5379862fe3a4711f8c6c199322210a32
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2020-08-19 13:17:09 +0300

    moved lexeme bookmark freeforms to lexeme tags

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -8,6 +8,7 @@ alter table lexeme drop column process_state_code cascade;
 drop table process_state cascade;
 create index lexeme_is_public_idx on lexeme(is_public);
 
+-- roheliste ilmikute detailsus sõltuvalt mustade detailsusest
 update lexeme l
 set complexity = l_c.complexity
 from (select ls.id lexeme_id,
@@ -23,3 +24,30 @@ from (select ls.id lexeme_id,
         and lp.word_id = ls.word_id
       group by ls.id) l_c
 where l.id = l_c.lexeme_id;
+
+-- bookmarkide teisendamine ilmiku siltideks
+insert into tag select distinct value_text from freeform where type = 'BOOKMARK';
+
+insert into lexeme_tag (lexeme_id, tag_name)
+select distinct lff.lexeme_id, ff.value_text
+from lexeme_freeform lff,
+     freeform ff
+where lff.freeform_id = ff.id
+  and ff.type = 'BOOKMARK'
+  and not exists(select lt.id
+                 from lexeme_tag lt
+                 where lt.lexeme_id = lff.lexeme_id
+                   and lt.tag_name = ff.value_text);
+
+update lifecycle_log lcl
+set entity_id = lff.lexeme_id,
+    entity_name = 'LEXEME',
+    entity_prop = 'TAG'
+from lexeme_freeform lff,
+     freeform ff
+where lff.freeform_id = ff.id
+  and ff.type = 'BOOKMARK'
+  and lcl.entity_id = ff.id
+  and lcl.entry = ff.value_text;
+
+delete from freeform where type = 'BOOKMARK';
