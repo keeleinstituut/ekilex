6b1b0c0f3a251c874291c4025baa7dbaf63313db
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2020-09-16 13:04:07 +0300

    sql script to set NOTE freeform lang

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -57,6 +57,37 @@ insert into aspect_label (code, value, lang, type) values ('сов.', 'сове�
 insert into aspect_label (code, value, lang, type) values ('несов.', 'несовершенный вид (что делать)', 'est', 'wordweb');
 insert into aspect_label (code, value, lang, type) values ('сов. и несов.', 'совершенный и несовершенный', 'est', 'wordweb');
 
+-- ilma keeleta märkustele keele määramine (tekkisid pärast Estermi laadimist)
+update freeform ff
+set lang = d.lang
+from definition_freeform dff,
+     definition d
+where ff.lang is null
+  and ff.type = 'NOTE'
+  and ff.id = dff.freeform_id
+  and dff.definition_id = d.id;
+
+update freeform ff
+set lang = w.lang
+from lexeme_freeform lff,
+     lexeme l,
+     word w
+where ff.lang is null
+  and ff.type = 'NOTE'
+  and ff.id = lff.freeform_id
+  and lff.lexeme_id = l.id
+  and l.word_id = w.id;
+
+update freeform ff
+set lang = case
+             when ff.value_text ilike 'note%' then 'eng'
+             else 'est'
+           end
+from meaning_freeform mff
+where ff.lang is null
+  and ff.type = 'NOTE'
+  and ff.id = mff.freeform_id;
+
 ---------------------------------------------
 -- lifecycle log to activity log migration --
 ---------------------------------------------
