eac8398ca02803f29b7e9a8980215820ddb0a623
Andre Luts <andre.luts@twn.ee>
2020-10-23 14:38:18 +0300

    mobile dropdown fixes, style improvements

--- wordweb-app/src/main/resources/view/js/aside-menu.js
+++ wordweb-app/src/main/resources/view/js/aside-menu.js
@@ -1,154 +1,175 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sideMenu = document.querySelector('.dropdown-aside')
+  $(document).ready(function () {
 
-  nestedList = $(sideMenu).find('li.has-submenu')
-  var mdBreakpoint = 768
-  $(nestedList).on('click', function (e) {
-    var target = $(e.target).closest('li')
-    if ($(target).hasClass('has-submenu')) {
-      if ($(target).hasClass('open')) {
-        $(target).removeClass('open')
-        $(e.target).attr("aria-expanded", false);
-      } else {
-        $(nestedList).removeClass('open')
-        $(target).addClass('open')
-        var tabID = $(target).find('a').attr('href')
-        $('.tab-pane').removeClass('show').removeClass('active')
-        $(tabID).tab('show')
-        $(e.target).attr("aria-expanded", true);
+    const sideMenu = document.querySelector('.dropdown-aside')
+    const sideMenuHeading = sideMenu.querySelector('.dropdown-toggle')
+    var mdBreakpoint = 768
 
-      }
+    function linkActive(targetHref) {
+      var targetHref = targetHref
+      $('.has-submenu').removeClass('open')
+      $('a[href="' + targetHref + '"]').addClass('active')
+      showPanel()
     }
-  })
 
+    let navLinks = sideMenu.querySelectorAll('.nav-link')
+    navLinks.forEach(function (el, i) {
 
-  let navLinks = sideMenu.querySelectorAll('.nav-link')
-  navLinks.forEach(function (el, i) {
+      el.addEventListener('click', (element) => {
+        element.preventDefault()
+        var dropdownTitle
 
-    el.addEventListener('click', (element) => {
-      element.preventDefault()
-      if ($(el).closest('ul').hasClass('dropdown-menu')) {
-        $('.tab-content').animate({
-          scrollTop: $(window).scrollTop(0)
-        })
-      }
+        targetHref = $(element.target).closest('.nav-link').attr('href');
+        location.hash = targetHref;
 
-      targetHref = $(element.target).attr('href');
-      location.hash = targetHref;
-
-      function linkActive() {
-        $('.nav-link').removeClass('active')
-        $('a[href="' + targetHref + '"]').addClass('active')
-      }
-
-      window.setTimeout(linkActive, 50);
-
-      if (!$(el).parents().hasClass('has-submenu'))
-        $('.has-submenu.open').removeClass('open')
-
-      const sideMenuHeading = sideMenu.querySelector('.dropdown-toggle')
-      var parentList = $(el).closest('.nav-link').closest('ul')
-      var dropdownTitle
-
-      if (!parentList.hasClass('dropdown-menu')) {
-        var subMenuLink = $(el).closest('ul').parent().find('.nav-link[data-toggle="pill"]')
-        if ($(window).width() < mdBreakpoint) {
-          $(subMenuLink).addClass('active')
+        if ($(el).closest('ul').hasClass('dropdown-menu')) {
+          $('.tab-content').animate({
+            scrollTop: $(window).scrollTop(0)
+          })
+        } else {
+          $('.nav-link').removeClass('active')
+          $(element.target).addClass('active')
+          showPanel()
+          return
         }
-        dropdownTitle = $(subMenuLink).contents().get(0).nodeValue
-      } else {
-        dropdownTitle = $(el).contents().get(0).nodeValue
-      }
 
-      sideMenuHeading.innerHTML = dropdownTitle
+        var navLinkIcon = $('.nav-link-icon')
+
+        $(navLinkIcon).on('click', function (element) {
+          var target = $(element.target).closest('.nav-link').closest('.has-submenu')
+          if ($(target).hasClass('open')) {
+            $(target).find('.nav-link').removeClass('active')
+            $(target).removeClass('open')
+            element.stopImmediatePropagation()
+          } else {
+            if ($(element.target).hasClass('nav-link-icon') && $(window).width() < mdBreakpoint) {
+              var targetLink = $(element.target).parent()
+              if ($(targetLink).hasClass('active')) {
+                targetLink.removeClass('active')
+                $(targetLink).parent().removeClass('open')
+                element.stopImmediatePropagation()
+                return
+              } else {
+                $('.nav-link.active').removeClass('active')
+                targetLink.addClass('active')
+                $(targetLink).parent().addClass('open')
+                element.stopImmediatePropagation()
+                return
+              }
+            }
+          }
+        })
+
+        window.setTimeout(linkActive(targetHref), 50);
+
+        if ($(el).parents().hasClass('has-submenu') && $(window).width() <= mdBreakpoint) {
+          return;
+        }
+
+        var parentList = $(el).closest('.nav-link').closest('ul')
+
+        if (!parentList.hasClass('dropdown-menu')) {
+          var subMenuLink = $(el).closest('ul').parent().find('.nav-link[data-toggle="pill"]')
+          $('.has-submenu.open').removeClass('open')
+          if ($(window).width() < mdBreakpoint) {
+            $(subMenuLink).addClass('active')
+          }
+        } else {
+          dropdownTitle = $(el).contents().get(0).nodeValue
+        }
+        sideMenuHeading.innerHTML = dropdownTitle
+      })
     })
-  })
 
-  function showPanel() {
-    if (window.location.pathname == '/learn') {
-      var hash = window.location.hash.substring(1)
-      var target = $('#' + hash)
-      var closestTab = $(target).closest('.tab-pane')
-      $('.nav-link' + hash).addClass('active')
-      $('.nav-link').attr('href')
-      var targetLink = $("a[href$='" + hash + "']")
-      $('.nav-link').removeClass('active')
-      targetLink.addClass('active')
+    function showPanel() {
+      if (window.location.pathname == '/learn') {
+        var hash = window.location.hash.substring(1)
+        if (hash == 'undefined') {
+          return;
+        } else {
+          var target = $('#' + hash)
+          var closestTab = $(target).closest('.tab-pane')
+          var targetLink = $("a[href$='" + hash + "']")
+          sideMenuHeading.innerHTML = targetLink.eq(0).text()
+          $('.nav-link').removeClass('active')
+          targetLink.eq(0).addClass('active')
+          if ($(targetLink).parent().hasClass('has-submenu')) {
+            $(targetLink).parent().toggleClass('open')
+          }
+          $('.tab-pane').removeClass('show').removeClass('active')
+          $(closestTab).addClass('show active')
 
-      if (!$(targetLink).closest('ul').hasClass('dropdown-menu')) {
-        $(targetLink).closest('ul').parent().addClass('open')
-      }
-      $('.tab-pane').removeClass('show').removeClass('active')
-      $(closestTab).addClass('show active')
-
-      setTimeout(() => {
-        
-        $('html').animate({
-          scrollTop: target.offset().top - 100
-        });
-      }, 300);
-    }
-
-  }
-  
-  window.setTimeout(showPanel, 0);
-
-
-  // sub-menu scrolling toggling active class
-  var findPos = function (obj) {
-    var curleft = 0,
-      curtop = 0;
-    if (obj.offsetParent) {
-      curleft = obj.offsetLeft;
-      curtop = obj.offsetTop;
-      while ((obj = obj.offsetParent)) {
-        curleft += obj.offsetLeft;
-        curtop += obj.offsetTop;
+          if ($(window).width() >= mdBreakpoint) {
+            setTimeout(() => {
+              $('html').animate({
+                scrollTop: target.offset().top - 100
+              });
+            }, 300);
+          }
+        }
       }
     }
-    return [curleft, curtop];
-  };
 
-  var findClosestAnchor = function (anchors) {
+    window.setTimeout(showPanel(), 0);
 
-    var sortByDistance = function (element1, element2) {
-      var pos1 = findPos(element1),
-        pos2 = findPos(element2);
-      var vect1 = [
-          window.scrollX - pos1[0],
-          window.scrollY - pos1[1]
-        ],
-        vect2 = [
-          window.scrollX - pos2[0],
-          window.scrollY - pos2[1]
-        ];
-
-      // we compare the length of the vectors using only the sum of their components squared
-      // no need to find the magnitude of each (this was inspired by Mageek’s answer)
-      var sqDist1 = vect1[0] * vect1[0] + vect1[1] * vect1[1],
-        sqDist2 = vect2[0] * vect2[0] + vect2[1] * vect2[1];
-
-      if (sqDist1 < sqDist2) return -1;
-      else if (sqDist1 > sqDist2) return 1;
-      else return 0;
+    // sub-menu scrolling toggling active class
+    var findPos = function (obj) {
+      var curleft = 0,
+        curtop = 0;
+      if (obj.offsetParent) {
+        curleft = obj.offsetLeft;
+        curtop = obj.offsetTop;
+        while ((obj = obj.offsetParent)) {
+          curleft += obj.offsetLeft;
+          curtop += obj.offsetTop;
+        }
+      }
+      return [curleft, curtop];
     };
 
-    return Array.prototype.slice.call(anchors).sort(sortByDistance)[0];
-  };
-  var anchors = document.body.querySelectorAll('.tab-content a[id]');
+    var findClosestAnchor = function (anchors) {
 
-  var onScroll = function (e) {
-    e.preventDefault();
-    var closest = findClosestAnchor(anchors);
-    var value = $(closest).attr('id')
-    $('.has-submenu.open').find('a').removeClass('active')
-    var activeLink = $('.has-submenu.open').find('a[href="#' + value + '"]')
-    $(activeLink).addClass('active')
-  };
+      var sortByDistance = function (element1, element2) {
+        var pos1 = findPos(element1),
+          pos2 = findPos(element2);
+        var vect1 = [
+            window.scrollX - pos1[0],
+            window.scrollY - pos1[1]
+          ],
+          vect2 = [
+            window.scrollX - pos2[0],
+            window.scrollY - pos2[1]
+          ];
 
-  window.addEventListener('scroll', function (e) {
-    if ($('.has-submenu.open').length && $(window).width() > mdBreakpoint) {
-      onScroll(e)
-    }
-  })
-})
+        // we compare the length of the vectors using only the sum of their components squared
+        // no need to find the magnitude of each (this was inspired by Mageek’s answer)
+        var sqDist1 = vect1[0] * vect1[0] + vect1[1] * vect1[1],
+          sqDist2 = vect2[0] * vect2[0] + vect2[1] * vect2[1];
+
+        if (sqDist1 < sqDist2) return -1;
+        else if (sqDist1 > sqDist2) return 1;
+        else return 0;
+      };
+
+      return Array.prototype.slice.call(anchors).sort(sortByDistance)[0];
+
+    };
+    var anchors = document.body.querySelectorAll('.tab-content a[id]');
+
+    var onScroll = function (e) {
+      var closest = findClosestAnchor(anchors);
+      var value = $(closest).attr('id')
+      $('.has-submenu.open').find('a').removeClass('active')
+      var activeLink = $('.has-submenu.open').find('a[href="#' + value + '"]')
+      if ($(activeLink).text() !== '') {
+        sideMenuHeading.innerHTML = ''
+        sideMenuHeading.innerHTML = $(activeLink).text()
+      }
+      $(activeLink).addClass('active')
+    };
+
+    window.addEventListener('wheel', function (e) {
+      if ($('.has-submenu.open').length && !$('.dropdown-aside.show').length) {
+        onScroll(e)
+      }
+    });
+  });
