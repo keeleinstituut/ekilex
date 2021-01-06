89c325aeec3f78efbadad95c000b7aeab3fdd2d5
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2021-01-06 14:42:07 +0200

    removed unnecessary ekistat indexes

--- ekistat-app/src/main/resources/sql/migration.sql
+++ ekistat-app/src/main/resources/sql/migration.sql
@@ -1,5 +1,6 @@
 create index ww_search_search_word_idx on ww_search(search_word);
 create index ww_search_search_mode_idx on ww_search(search_mode);
-create index ww_search_destin_langs_idx on ww_search(destin_langs);
-create index ww_search_dataset_codes_idx on ww_search(dataset_codes);
 create index ww_search_event_on_idx on ww_search(event_on);
+
+drop index if exists ww_search_destin_langs_idx;
+drop index if exists ww_search_dataset_codes_idx;
