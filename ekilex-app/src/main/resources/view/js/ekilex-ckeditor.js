48954807eeb8df850691a6c718b2f7880974cf43
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2021-05-05 18:22:59 +0300

    updated ckeditor api service url

--- ekilex-app/src/main/resources/view/js/ekilex-ckeditor.js
+++ ekilex-app/src/main/resources/view/js/ekilex-ckeditor.js
@@ -123,12 +123,12 @@ class ckLink {
 		this.parentHeight = this.parent.outerHeight();
 		this.paths = {
 			meaning: {
-				api: `${applicationBaseUrl}/meaning_internal_link_search`,
+				api: `${applicationUrl}/meaning_internal_link_search`,
 				link: `${applicationBaseUrl}/lexsearch?id={{id}};`,
 				title: 'Sisesta otsitav Keelend',
 			},
 			word: {
-				api: `${applicationBaseUrl}/word_internal_link_search`,
+				api: `${applicationUrl}/word_internal_link_search`,
 				link: `${applicationBaseUrl}/termsearch?id={{id}};`,
 				title: 'Sisesta otsitav Tähendus/Mõiste',
 			},
