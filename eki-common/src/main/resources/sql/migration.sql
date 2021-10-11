0edec87f997d0001ffd4db76e66fbc0836107d25
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2021-10-11 17:45:30 +0300

    updated html entity codes replacing

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -40,123 +40,72 @@ end $$;
 -- ilmiku usaldusväärsus
 alter table lexeme add column reliability integer null;
 
--- html entity parandused
-update freeform set value_text = replace(value_text, '&otilde;', 'õ') where value_text like '%&otilde;%';
-update freeform set value_prese = replace(value_prese, '&otilde;', 'õ') where value_prese like '%&otilde;%';
-update freeform set value_text = replace(value_text, '&Otilde;', 'Õ') where value_text like '%&Otilde;%';
-update freeform set value_prese = replace(value_prese, '&Otilde;', 'Õ') where value_prese like '%&Otilde;%';
-update definition set value = replace(value, '&otilde;', 'õ') where value like '%&otilde;%';
-update definition set value_prese = replace(value_prese, '&otilde;', 'õ') where value_prese like '%&otilde;%';
-update definition set value = replace(value, '&Otilde;', 'Õ') where value like '%&Otilde;%';
-update definition set value_prese = replace(value_prese, '&Otilde;', 'Õ') where value_prese like '%&Otilde;%';
-
-update freeform set value_text = replace(value_text, '&auml;', 'ä') where value_text like '%&auml;%';
-update freeform set value_prese = replace(value_prese, '&auml;', 'ä') where value_prese like '%&auml;%';
-update freeform set value_text = replace(value_text, '&Auml;', 'Ä') where value_text like '%&Auml;%';
-update freeform set value_prese = replace(value_prese, '&Auml;', 'Ä') where value_prese like '%&Auml;%';
-update definition set value = replace(value, '&auml;', 'ä') where value like '%&auml;%';
-update definition set value_prese = replace(value_prese, '&auml;', 'ä') where value_prese like '%&auml;%';
-update definition set value = replace(value, '&Auml;', 'Ä') where value like '%&Auml;%';
-update definition set value_prese = replace(value_prese, '&Auml;', 'Ä') where value_prese like '%&Auml;%';
-
-update freeform set value_text = replace(value_text, '&ouml;', 'ö') where value_text like '%&ouml;%';
-update freeform set value_prese = replace(value_prese, '&ouml;', 'ö') where value_prese like '%&ouml;%';
-update freeform set value_text = replace(value_text, '&Ouml;', 'Ö') where value_text like '%&Ouml;%';
-update freeform set value_prese = replace(value_prese, '&Ouml;', 'Ö') where value_prese like '%&Ouml;%';
-update definition set value = replace(value, '&ouml;', 'ö') where value like '%&ouml;%';
-update definition set value_prese = replace(value_prese, '&ouml;', 'ö') where value_prese like '%&ouml;%';
-update definition set value = replace(value, '&Ouml;', 'Ö') where value like '%&Ouml;%';
-update definition set value_prese = replace(value_prese, '&Ouml;', 'Ö') where value_prese like '%&Ouml;%';
-
-update freeform set value_text = replace(value_text, '&uuml;', 'ü') where value_text like '%&uuml;%';
-update freeform set value_prese = replace(value_prese, '&uuml;', 'ü') where value_prese like '%&uuml;%';
-update freeform set value_text = replace(value_text, '&Uuml;', 'Ü') where value_text like '%&Uuml;%';
-update freeform set value_prese = replace(value_prese, '&Uuml;', 'Ü') where value_prese like '%&Uuml;%';
-update definition set value = replace(value, '&uuml;', 'ü') where value like '%&uuml;%';
-update definition set value_prese = replace(value_prese, '&uuml;', 'ü') where value_prese like '%&uuml;%';
-update definition set value = replace(value, '&Uuml;', 'Ü') where value like '%&Uuml;%';
-update definition set value_prese = replace(value_prese, '&Uuml;', 'Ü') where value_prese like '%&Uuml;%';
-
-update freeform set value_text = replace(value_text, '&scaron;', 'š') where value_text like '%&scaron;%';
-update freeform set value_prese = replace(value_prese, '&scaron;', 'š') where value_prese like '%&scaron;%';
-update freeform set value_text = replace(value_text, '&Scaron;', 'Š') where value_text like '%&Scaron;%';
-update freeform set value_prese = replace(value_prese, '&Scaron;', 'Š') where value_prese like '%&Scaron;%';
-update definition set value = replace(value, '&scaron;', 'š') where value like '%&scaron;%';
-update definition set value_prese = replace(value_prese, '&scaron;', 'š') where value_prese like '%&scaron;%';
-update definition set value = replace(value, '&Scaron;', 'Š') where value like '%&Scaron;%';
-update definition set value_prese = replace(value_prese, '&Scaron;', 'Š') where value_prese like '%&Scaron;%';
-
-update freeform set value_text = replace(value_text, '&ntilde;', 'ñ') where value_text like '%&ntilde;%';
-update freeform set value_prese = replace(value_prese, '&ntilde;', 'ñ') where value_prese like '%&ntilde;%';
-update definition set value = replace(value, '&ntilde;', 'ñ') where value like '%&ntilde;%';
-update definition set value_prese = replace(value_prese, '&ntilde;', 'ñ') where value_prese like '%&ntilde;%';
-
-update freeform set value_text = replace(value_text, '&quot;', '"') where value_text like '%&quot;%';
-update freeform set value_prese = replace(value_prese, '&quot;', '"') where value_prese like '%&quot;%';
-update definition set value = replace(value, '&quot;', '"') where value like '%&quot;%';
-update definition set value_prese = replace(value_prese, '&quot;', '"') where value_prese like '%&quot;%';
-
-update freeform set value_text = replace(value_text, '&ndash;', '–') where value_text like '%&ndash;%';
-update freeform set value_prese = replace(value_prese, '&ndash;', '–') where value_prese like '%&ndash;%';
-update definition set value = replace(value, '&ndash;', '–') where value like '%&ndash;%';
-update definition set value_prese = replace(value_prese, '&ndash;', '–') where value_prese like '%&ndash;%';
-
-update freeform set value_text = replace(value_text, '&gt;', '>') where value_text like '%&gt;%';
-update freeform set value_prese = replace(value_prese, '&gt;', '>') where value_prese like '%&gt;%';
-update definition set value = replace(value, '&gt;', '>') where value like '%&gt;%';
-update definition set value_prese = replace(value_prese, '&gt;', '>') where value_prese like '%&gt;%';
-
-update freeform set value_text = replace(value_text, '&lt;', '<') where value_text like '%&lt;%';
-update freeform set value_prese = replace(value_prese, '&lt;', '<') where value_prese like '%&lt;%';
-update definition set value = replace(value, '&lt;', '<') where value like '%&lt;%';
-update definition set value_prese = replace(value_prese, '&lt;', '<') where value_prese like '%&lt;%';
-
-update freeform set value_text = replace(value_text, '&#39;', '''') where value_text like '%&#39;%';
-update freeform set value_prese = replace(value_prese, '&#39;', '''') where value_prese like '%&#39;%';
-update definition set value = replace(value, '&#39;', '''') where value like '%&#39;%';
-update definition set value_prese = replace(value_prese, '&#39;', '''') where value_prese like '%&#39;%';
-
-update freeform set value_text = replace(value_text, '&ldquo;', '“') where value_text like '%&ldquo;%';
-update freeform set value_prese = replace(value_prese, '&ldquo;', '“') where value_prese like '%&ldquo;%';
-update definition set value = replace(value, '&ldquo;', '“') where value like '%&ldquo;%';
-update definition set value_prese = replace(value_prese, '&ldquo;', '“') where value_prese like '%&ldquo;%';
-
-update freeform set value_text = replace(value_text, '&rdquo;', '”') where value_text like '%&rdquo;%';
-update freeform set value_prese = replace(value_prese, '&rdquo;', '”') where value_prese like '%&rdquo;%';
-update definition set value = replace(value, '&rdquo;', '”') where value like '%&rdquo;%';
-update definition set value_prese = replace(value_prese, '&rdquo;', '”') where value_prese like '%&rdquo;%';
-
-update freeform set value_text = replace(value_text, '&lsquo;', '‘') where value_text like '%&lsquo;%';
-update freeform set value_prese = replace(value_prese, '&lsquo;', '‘') where value_prese like '%&lsquo;%';
-update definition set value = replace(value, '&lsquo;', '‘') where value like '%&lsquo;%';
-update definition set value_prese = replace(value_prese, '&lsquo;', '‘') where value_prese like '%&lsquo;%';
-
-update freeform set value_text = replace(value_text, '&rsquo;', '’') where value_text like '%&rsquo;%';
-update freeform set value_prese = replace(value_prese, '&rsquo;', '’') where value_prese like '%&rsquo;%';
-update definition set value = replace(value, '&rsquo;', '’') where value like '%&rsquo;%';
-update definition set value_prese = replace(value_prese, '&rsquo;', '’') where value_prese like '%&rsquo;%';
-
-update freeform set value_text = replace(value_text, '&laquo;', '«') where value_text like '%&laquo;%';
-update freeform set value_prese = replace(value_prese, '&laquo;', '«') where value_prese like '%&laquo;%';
-update definition set value = replace(value, '&laquo;', '«') where value like '%&laquo;%';
-update definition set value_prese = replace(value_prese, '&laquo;', '«') where value_prese like '%&laquo;%';
-
-update freeform set value_text = replace(value_text, '&raquo;', '»') where value_text like '%&raquo;%';
-update freeform set value_prese = replace(value_prese, '&raquo;', '»') where value_prese like '%&raquo;%';
-update definition set value = replace(value, '&raquo;', '»') where value like '%&raquo;%';
-update definition set value_prese = replace(value_prese, '&raquo;', '»') where value_prese like '%&raquo;%';
-
-update freeform set value_text = replace(value_text, '&bdquo;', '„') where value_text like '%&bdquo;%';
-update freeform set value_prese = replace(value_prese, '&bdquo;', '„') where value_prese like '%&bdquo;%';
-update definition set value = replace(value, '&bdquo;', '„') where value like '%&bdquo;%';
-update definition set value_prese = replace(value_prese, '&bdquo;', '„') where value_prese like '%&bdquo;%';
-
-update freeform set value_text = replace(value_text, '&nbsp;', ' ') where value_text like '%&nbsp;%';
-update freeform set value_prese = replace(value_prese, '&nbsp;', ' ') where value_prese like '%&nbsp;%';
-update definition set value = replace(value, '&nbsp;', ' ') where value like '%&nbsp;%';
-update definition set value_prese = replace(value_prese, '&nbsp;', ' ') where value_prese like '%&nbsp;%';
-update word set value = replace(value, '&nbsp;', ' ') where value like '%&nbsp;%';
-update word set value_prese = replace(value_prese, '&nbsp;', ' ') where value_prese like '%&nbsp;%';
+-- html entity asendamised
+do $$
+declare
+  html_entity           text;
+  sym                   text;
+  html_entity_sym_row   text[];
+  html_entity_sym_array text[][] := array [
+    ['&otilde;','õ'],['&Otilde;','Õ'],
+    ['&auml;','ä'],['&Auml;','Ä'],
+    ['&ouml;','ö'],['&Ouml;','Ö'],
+    ['&uuml;','ü'],['&Uuml;','Ü'],
+    ['&scaron;','š'],['&Scaron;','Š'],
+    ['&ocirc;','ô'],['&Ocirc;','Ô'],
+    ['&oacute;','ó'],['&Oacute;','Ó'],
+    ['&ograve;','ò'],['&Ograve;','Ò'],
+    ['&ocirc;','ô'],['&Ocirc;','Ô'],
+    ['&ntilde;','ñ'],['&Ntilde;','Ñ'],
+    ['&quot;','"'],['&#39;',''''],['&bdquo;','„'],
+    ['&ldquo;','“'],['&rdquo;','”'],
+    ['&lsquo;','‘'],['&rsquo;','’'],
+    ['&laquo;','«'],['raquo','»'],
+    ['&ndash;','–'],
+    ['&gt;','>'],['&lt;','<'],
+    ['&nbsp;',' '],
+    ['&alpha;','α'],['&Alpha;','Α'],
+    ['&beta;','β'],['&Beta;','Β'],
+    ['&gamma;','γ'],['&Gamma;','Γ'],
+    ['&delta;','δ'],['&Delta;','Δ'],
+    ['&epsilon;','ε'],['&Epsilon;','Ε'],
+    ['&zeta;','ζ'],['&Zeta;','Ζ'],
+    ['&eta;','η'],['&Eta;','Η'],
+    ['&theta;','θ'],['&Theta;','Θ'],
+    ['&iota;','ι'],['&Iota;','Ι'],
+    ['&kappa;','κ'],['&Kappa;','Κ'],
+    ['&lambda;','λ'],['&Lambda;','Λ'],
+    ['&mu;','μ'],['&Mu;','Μ'],
+    ['&nu;','ν'],['&Nu;','Ν'],
+    ['&xi;','ξ'],['&Xi;','Ξ'],
+    ['&omicron;','ο'],['&Omicron;','Ο'],
+    ['&pi;','π'],['&Pi;','Π'],
+    ['&Rho;','ρ'],['&Zeta;','Ρ'],
+    ['&sigmaf;','ς'],
+    ['&sigma;','σ'],['&Sigma;','Σ'],
+    ['&tau;','τ'],['&Tau;','Τ'],
+    ['&upsilon;','υ'],['&Upsilon;','Υ'],
+    ['&phi;','φ'],['&Phi;','Φ'],
+    ['&chi;','χ'],['&Chi;','Χ'],
+    ['&psi;','ψ'],['&Psi;','Ψ'],
+    ['&omega;','ω'],['&Omega;','Ω'],
+    ['&hellip;','…'],
+    ['&micro;','µ'],
+    ['&minus;','−']];
+begin
+  foreach html_entity_sym_row slice 1 in array html_entity_sym_array
+    loop
+      html_entity := html_entity_sym_row[1];
+      sym := html_entity_sym_row[2];
+      update freeform set value_text = replace(value_text, html_entity, sym) where value_text like '%'||html_entity||'%';
+      update freeform set value_prese = replace(value_prese, html_entity, sym) where value_prese like '%'||html_entity||'%';
+      update definition set value = replace(value, html_entity, sym) where value like '%'||html_entity||'%';
+      update definition set value_prese = replace(value_prese, html_entity, sym) where value_prese like '%'||html_entity||'%';
+      update word set value = replace(value, html_entity, sym) where value like '%'||html_entity||'%';
+      update word set value_prese = replace(value_prese, html_entity, sym) where value_prese like '%'||html_entity||'%';
+      raise notice 'replaced % to %',html_entity, sym;
+    end loop;
+end $$;
 
 -- eemaldab tühikud keelendi ees ja lõpus
 update word set value = trim(value), value_prese = trim(value_prese);
