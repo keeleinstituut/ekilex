fb13c6c2b648ceb546134e3a3e197fe4086ae931
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2022-06-27 14:51:54 +0300

    migrated existing freeform word and meaning links to the new format

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -4,3 +4,8 @@ update definition set value_prese = replace(value_prese, ' link-id=', ' data-lin
 update definition set value_prese = replace(value_prese, ' link-type=', ' data-link-type=') where value_prese like '%eki-link%';
 update definition set value_prese = replace(value_prese, ' data-link-type=''meaning_link''', ' data-link-type="meaning"') where value_prese like '%eki-link%';
 update definition set value_prese = replace(value_prese, ' data-link-type="meaning_link"', ' data-link-type="meaning"') where value_prese like '%eki-link%';
+
+update freeform set value_prese = replace(value_prese, ' id=', ' data-link-id=') where value_prese like '%eki-link%' and value_prese like '%type="meaning"%';
+update freeform set value_prese = replace(value_prese, ' link-type=', ' data-link-type=') where value_prese like '%eki-link%' and value_prese like '%type="meaning"%';
+update freeform set value_prese = replace(value_prese, ' id=', ' data-link-id=') where value_prese like '%eki-link%' and value_prese like '%type="word"%';
+update freeform set value_prese = replace(value_prese, ' link-type=', ' data-link-type=') where value_prese like '%eki-link%' and value_prese like '%type="word"%';
