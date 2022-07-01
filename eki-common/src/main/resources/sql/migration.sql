3c765d2ece3b1c5a2e4d6e570d0949363d6d281a
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2022-07-01 11:28:26 +0300

    deleted lexeme and usage od recommendations

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -11,15 +11,5 @@ update freeform set value_prese = replace(value_prese, ' id=', ' data-link-id=')
 update freeform set value_prese = replace(value_prese, ' link-type=', ' data-link-type=') where value_prese like '%eki-link%' and value_prese like '%type="word"%';
 
 -- ilmiku ja kasutusnäite ÕS soovituste kustutamine
-drop type if exists type_usage;
-create type type_usage as (
-				usage_id bigint,
-				usage text,
-				usage_prese text,
-				usage_lang char(3),
-				complexity varchar(100),
-				usage_type_code varchar(100),
-				usage_translations text array,
-				usage_definitions text array);
-
 delete from freeform where type in ('OD_LEXEME_RECOMMENDATION', 'OD_USAGE_DEFINITION', 'OD_USAGE_ALTERNATIVE');
+-- recreate ekilex views and types
