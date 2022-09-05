22928c068c8fa5a02d2b5d5f93a677b8bce2c12a
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2022-09-05 18:16:25 +0300

    updated database init for running tests

--- eki-common/src/main/java/eki/common/test/TestEnvInitialiser.java
+++ eki-common/src/main/java/eki/common/test/TestEnvInitialiser.java
@@ -31,12 +31,13 @@ public class TestEnvInitialiser {
 
 		final String scriptFilePath1 = "sql/drop_all.sql";
 		final String scriptFilePath2 = "sql/create_tables.sql";
-		//final String scriptFilePath3 = "sql/create_views.sql";
-		final String scriptFilePath4 = "sql/classifier-manual.sql";
-		final String scriptFilePath5 = "sql/classifier-main.sql";
-		final String scriptFilePath6 = "sql/classifier-domain.sql";
+		// final String scriptFilePath3 = "sql/create_views.sql";
+		final String scriptFilePath4 = "sql/create_functions.sql";
+		final String scriptFilePath5 = "sql/classifier-manual.sql";
+		final String scriptFilePath6 = "sql/classifier-main.sql";
+		final String scriptFilePath7 = "sql/classifier-domain.sql";
 		final String[] scriptFilePaths = new String[] {
-				scriptFilePath1, scriptFilePath2, /*scriptFilePath3, */scriptFilePath4, scriptFilePath5, scriptFilePath6
+				scriptFilePath1, scriptFilePath2, /*scriptFilePath3, */scriptFilePath4, scriptFilePath5, scriptFilePath6, scriptFilePath7
 		};
 
 		for (String scriptFilePath : scriptFilePaths) {
