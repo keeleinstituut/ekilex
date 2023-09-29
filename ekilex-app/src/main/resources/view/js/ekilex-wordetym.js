15dbfffb05f21a69b21f2f67c9ba838f1ff3c258
Joel Randveer <joel.randveer@twn.ee>
2023-09-29 11:04:23 +0300

    Etym tree logic update

--- ekilex-app/src/main/resources/view/js/ekilex-wordetym.js
+++ ekilex-app/src/main/resources/view/js/ekilex-wordetym.js
@@ -1,14 +1,14 @@
 $(function() {
   // $('.btn-edit-etym').off('click').on('click', function(e) {
-	// 	e.preventDefault();
-	// 	var obj = $(this);
-	// 	var linksBlock = obj.parent();
-	// 	var container = obj.parents().find('.wordetym-card');
-		
-	// 	obj.toggleClass('active');
-	// 	linksBlock.toggleClass('active');
-	// 	container.toggleClass('wordetym-card--active');
-	// });
+  //  e.preventDefault();
+  //  var obj = $(this);
+  //  var linksBlock = obj.parent();
+  //  var container = obj.parents().find('.wordetym-card');
+    
+  //  obj.toggleClass('active');
+  //  linksBlock.toggleClass('active');
+  //  container.toggleClass('wordetym-card--active');
+  // });
 });
 
 $.fn.wordetymCard = function() {
@@ -26,6 +26,20 @@ $.fn.wordetymCard = function() {
       var btn = $(this);
       btn.toggleClass('active');
       obj.toggleClass('add-edit-open');
+    });
+  });
+}
+
+$.fn.etymLinkModifier = function() {
+  return this.each(function(){
+    var obj = $(this);
+    var wrapper = $('.wordetym-wrapper');
+    var card = obj.parent().find('.wordetym-card');
+
+    obj.off('click').on('click', function() {
+      obj.toggleClass('active');
+      wrapper.toggleClass('modifier-active');
+      card.toggleClass('wordetym-card--active');
     });
   });
 }
