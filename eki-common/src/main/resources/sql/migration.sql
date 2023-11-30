4c2f2ba82840c57cf450d52c09f9292ed46a8a0e
Yogesh Sharma <yogesh.sharma@tripledev.ee>
2023-11-30 19:26:35 +0200

    added migration for 'author' type word etymology source links

--- eki-common/src/main/resources/sql/migration.sql
+++ eki-common/src/main/resources/sql/migration.sql
@@ -1,2 +1,3 @@
--- Kasutusnäite autori ja tõlkija parameetrist loobumine
+-- Allikaviidetes autori ja tõlkija parameetrist loobumine
 update freeform_source_link set type = 'ANY' where type in ('AUTHOR', 'TRANSLATOR');
+update word_etymology_source_link set type = 'ANY' where type = 'AUTHOR';
