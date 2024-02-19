b312d54b2bf9f5db0d91e2b4c38c649cd03a8912
Joel Randveer <joel.randveer@twn.ee>
2024-02-19 10:43:33 +0200

    Etym tree new structure

--- ekilex-app/src/main/resources/view/proto/tree-boxes.js
+++ ekilex-app/src/main/resources/view/proto/tree-boxes.js
@@ -1,215 +1,190 @@
 var i = 1;
-var levelWidth = 270;
-var nodeWidth = 200;
 
 function initWordEtymTree() {
+  var wordId = $("#word-id").val();
+  var applicationUrl = $("#app-url").val();
+  var wordEtymTreeUrl = applicationUrl + "proto/wordetymtree/" + wordId;
 
-	var wordId = $("#word-id").val();
-	var applicationUrl = $("#app-url").val();
-	var wordEtymTreeUrl = applicationUrl + "proto/wordetymtree/" + wordId;
-
-	$.get(wordEtymTreeUrl).done(function(data) {
-		//console.log(JSON.stringify(data, undefined, 4));
-		setup(data);
-	}).fail(function(data) {
-		console.log(data);
-	});
+  $.get(wordEtymTreeUrl).done(function(data) {
+    //console.log(JSON.stringify(data, undefined, 4));
+    setup(data);
+  }).fail(function(data) {
+    console.log(data);
+  });
 }
 
 function setup(data) {
+  var maxHeight = data.maxLevel * 100;
+  var margin = { top: 20, right: 10, bottom: 30, left: 10 };
+  var width = 1000 - margin.left - margin.right;
+  var height = maxHeight + margin.top + margin.bottom;
 
-	var maxWidth = data.maxLevel * levelWidth;
+  var svg = d3
+    .select("div[id='tree-container']")
+    .append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    ;
 
-	var margin = { top: 20, right: 10, bottom: 30, left: 10 };
-	var width = maxWidth - margin.left - margin.right;
-	var height = 800 - margin.top - margin.bottom;
+  var svgg = svg
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + 100 + ")")
+    ;
 
-	var svg = d3
-		.select("div[id='tree-container']")
-		.append("svg")
-		.attr("width", width + margin.right + margin.left)
-		.attr("height", height + margin.top + margin.bottom);
+  var tree = d3.tree().size([550, 120]);
 
-	var defs = svg.append("defs");
-	var marker = defs
-		.append("marker")
-		.attr("id", "arrow")
-		.attr("viewBox", "-10 -10 20 20")
-		.attr("refX", "10")
-		.attr("refY", "0")
-		.attr("markerWidth", "10")
-		.attr("markerHeight", "10")
-		.attr("class", "arrow")
-		.attr("orient", "auto");
-	marker
-		.append("path")
-		.attr("d", "M0,-10L10,0L0,10");
+  var root = d3.hierarchy(data.root, function(d) {
+    return d.children;
+  });
 
-	var svgg = svg
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  root.x0 = width / 2;
+  root.y0 = 0;
 
-	var tree = d3.tree().size([height, width]);
-	var root = d3.hierarchy(data.root, function(d) {
-		return d.children;
-	});
-	root.x0 = height / 2;
-	root.y0 = 0;
+  var treeData = tree(root);
+  var nodes = treeData.descendants()
 
-	var treeData = tree(root);
-	var nodes = treeData.descendants();
+  var nodeMap = {};
+  nodes.forEach(function(d) {
+    console.log(d)
+    d.y = d.depth * 90;
+    nodeMap[d.data.wordId] = d;
+  });
 
-	var nodeMap = {};
-	nodes.forEach(function(d) {
-		// nodes horiz distance
-		d.y = d.depth * levelWidth;
-		nodeMap[d.data.wordId] = d;
-	});
+  var links = data.links;
+  links.forEach(function(d) {
+    var sn = nodeMap[d.sourceWordId];
+    var tn = nodeMap[d.targetWordId];
+    d.s_x = sn.x;
+    d.s_y = sn.y;
+    d.t_x = tn.x;
+    d.t_y = tn.y;
+  })
+  ;
 
-	var links = data.links;
-	links.forEach(function(d) {
-		var sn = nodeMap[d.sourceWordId];
-		var tn = nodeMap[d.targetWordId];
-		d.s_x = sn.x;
-		d.s_y = sn.y;
-		d.t_x = tn.x;
-		d.t_y = tn.y;
-	});
-
-	render(svgg, root, nodes, links);
+  render(svgg, root, nodes, links);
 }
 
 function render(svgg, root, nodes, links) {
+  // -- nodes --
+  var node = svgg.selectAll("g.node").data(nodes, function(d) {return d.id || (d.id = ++i); });
 
-	// -- nodes --
+  // add node
+  var nodeEnter = node
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) {
+      return "translate(" + root.x0 + "," + root.y0 + ")";
+    })
+    .on("click", clickNode)
+    ;
 
-	var node = svgg.selectAll("g.node").data(nodes, function(d) {
-		i++;
-		if (d.id == undefined) {
-			d.id = i;
-		}
-		return d.id;
-	});
+  // path lang
+  nodeEnter
+    .append("path")
+    .attr("d", "m -0.1 7.9 c 0 -4.4 3.4 -8 7.6 -8 h 45.4 v 40 h -45.4 c -4.2 0 -7.6 -3.6 -7.6 -8")
+    .attr("fill", "#D7E5F2")
+    ;
 
-	// add node
-	var nodeEnter = node
-		.enter()
-		.append("g")
-		.attr("class", "node")
-		.attr("transform", function(d) {
-			return "translate(" + root.y0 + "," + root.x0 + ")";
-		})
-		.on("click", clickNode);
+  // text lang
+  nodeEnter
+    .append("text")
+    .style("fill", "#173148")
+    .attr("dy", 26)
+    .attr("x", 26)
+    .attr("text-anchor", function(d) {
+      return "middle";
+    })
+    .text(function(d) {
+      return d.data.wordLang;
+    })
+    ;
 
-	// rect word
-	nodeEnter
-		.append("rect")
-		.attr("rx", function(d) {
-			if (d.data.level == 1) {
-				return 10;
-			};
-			return 0;
-		})
-		.attr("ry", function(d) {
-			if (d.data.level == 1) {
-				return 10;
-			};
-			return 0;
-		})
-		.attr("x", 0)
-		.attr("y", -30)
-		.attr("width", nodeWidth)
-		.attr("height", 60)
-		.attr("stroke", "#ccd9e0")
-		.attr("stroke-width", 3)
-		.attr("fill", "none")
-		;
+  // text word
+  nodeEnter
+    .append("text")
+    .attr("class", "word-text")
+    .style("fill", "#173148")
+    .attr("dy", 26)
+    .attr("x", 60)
+    .attr("text-anchor", function(d) {
+      return "start";
+    })
+    .text(function(d) {
+      return d.data.wordValue;
+    })
+    ;
 
-	// rect lang
-	nodeEnter
-		.append("rect")
-		.attr("x", 10)
-		.attr("y", -20)
-		.attr("width", 40)
-		.attr("height", 40)
-		.attr("fill", "#ccd9e0")
-		;
+  // rect word
+  nodeEnter
+    .append("rect")
+    .attr("class", "node-border")
+    .attr("rx", 7)
+    .attr("ry", 7)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("height", 40)
+    .attr("width", function(d){
+      var wordTextElement = d3.select(this.parentNode).select(".word-text").node();
+      var textWidth = Math.round(wordTextElement.getBBox().width);
+      return textWidth + 70;
+    })
+    .attr("stroke", "#D7E5F2")
+    .attr("stroke-width", 3)
+    .attr("fill", "none")
+    ;
 
-	// text word
-	nodeEnter
-		.append("text")
-		.style("fill", "rgb(150, 150, 150)")
-		.attr("dy", ".35em")
-		.attr("x", 60)
-		.attr("text-anchor", function(d) {
-			return "start";
-		})
-		.text(function(d) {
-			return d.data.wordValue;
-		});
+  var nodeUpdate = nodeEnter.merge(node);
 
-	// text lang
-	nodeEnter
-		.append("text")
-		.style("fill", "rgb(150, 150, 150)")
-		.attr("dy", ".35em")
-		.attr("x", 30)
-		.attr("text-anchor", function(d) {
-			return "middle";
-		})
-		.text(function(d) {
-			return d.data.wordLang;
-		});
+  nodeUpdate
+    .attr("transform", function(d) {
+      var nodeLength = d3.select(this).select(".node-border").attr("width");
+      var position = Math.round(nodeLength / 4);
+      return "translate(" + ((d.x + position)) + "," + d.y + ")";
+    })
+    ;
 
-	var nodeUpdate = nodeEnter.merge(node);
+  // -- links --
+  var link = svgg.selectAll("path.link").data(links, function(d) {
+    return d.id;
+  });
 
-	nodeUpdate
-		.attr("transform", function(d) {
-			return "translate(" + d.y + "," + d.x + ")";
-		});
+  var linkEnter = link
+    .enter()
+    .insert("path", "g")
+    .attr("class", "link")
+    .attr("d", function(d) {
+      var o = { x: root.x0, y: root.y0 };
+      return diagonal(o, o);
+    })
+    .on("click", clickLink)
+    ;
 
-	// -- links --
+  var linkUpdate = linkEnter.merge(link);
 
-	var link = svgg.selectAll("path.link").data(links, function(d) {
-		return d.id;
-	});
+  linkUpdate
+    .attr("d", function(d) {
+      var sn = { x: d.s_x, y: d.s_y };
+      var tn = { x: d.t_x, y: d.t_y };
+      return diagonal(sn, tn);
+    });
 
-	var linkEnter = link
-		.enter()
-		.insert("path", "g")
-		.attr("class", "link")
-		.attr("d", function(d) {
-			var o = { x: root.x0, y: root.y0 };
-			return diagonal(o, o);
-		})
-		.attr("marker-end", "url(#arrow)")
-		.on("click", clickLink);
+  // link curviture
+  function diagonal(s, d) {
+    path = `M ${s.x + 100} ${s.y + 40}
+            C ${s.x + 100} ${(s.y + d.y + 40) / 2},
+              ${d.x + 100} ${(s.y + d.y + 40) / 2},
+              ${d.x + 100} ${d.y}`;
+    return path;
+  }
 
-	var linkUpdate = linkEnter.merge(link);
+  function clickNode(d) {
+    var modalId = "word-etym-node-edit-modal-" + d.data.wordId;
+    $("#" + modalId).modal();
+  }
 
-	linkUpdate
-		.attr("d", function(d) {
-			var sn = { x: d.s_x, y: d.s_y };
-			var tn = { x: d.t_x, y: d.t_y };
-			return diagonal(sn, tn);
-		});
-
-	// link curviture
-	function diagonal(s, d) {
-		path = `M ${s.y + nodeWidth} ${s.x}
-            C ${(s.y + d.y + nodeWidth) / 2} ${s.x},
-              ${(s.y + d.y + nodeWidth) / 2} ${d.x},
-              ${d.y - 3} ${d.x}`;
-		return path;
-	}
-
-	function clickNode(d) {
-		var modalId = "word-etym-node-edit-modal-" + d.data.wordId;
-		$("#" + modalId).modal();
-	}
-
-	function clickLink(d) {
-		var modalId = "word-etym-link-edit-modal-" + d.wordEtymRelId;
-		$("#" + modalId).modal();
-	}
+  function clickLink(d) {
+    var modalId = "word-etym-link-edit-modal-" + d.wordEtymRelId;
+    $("#" + modalId).modal();
+  }
 }
