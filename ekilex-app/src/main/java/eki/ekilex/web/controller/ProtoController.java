c22d96e66e99f7af19b12202eb668909357d213e
Martin Laubre <martin.laubre@gmail.com>
2024-09-25 18:11:10 +0300

    front layer tech poc. added proto permissions page service

--- ekilex-app/src/main/java/eki/ekilex/web/controller/ProtoController.java
+++ ekilex-app/src/main/java/eki/ekilex/web/controller/ProtoController.java
@@ -1,14 +1,24 @@
 package eki.ekilex.web.controller;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import eki.common.constant.OrderingField;
 import eki.ekilex.constant.WebConstant;
+import eki.ekilex.data.EkiUserPermData;
 import eki.ekilex.data.WordEtymTree;
+import eki.ekilex.data.proto.PermPageModel;
+import eki.ekilex.service.PermissionService;
 import eki.ekilex.service.WordEtymService;
 
 @Controller
@@ -16,6 +26,9 @@ public class ProtoController implements WebConstant {
 
 	@Autowired
 	private WordEtymService wordEtymService;
+
+	@Autowired
+	private PermissionService permissionService;
 
 	@GetMapping(PROTO_URI + "/etym")
 	public String initEtymPage() {
@@ -39,5 +52,41 @@ public class ProtoController implements WebConstant {
 		WordEtymTree wordEtymTree = wordEtymService.getWordEtymTree(wordId);
 
 		return wordEtymTree;
+	}
+
+	@PreAuthorize("isAuthenticated() && @permEval.isPrivatePageAccessPermitted(authentication)")
+	@PostMapping(PROTO_URI + PERMISSIONS_URI + SEARCH_URI)
+	@ResponseBody
+	public PermPageModel permSearch(
+			@RequestParam(name = "userNameFilter", required = false) String userNameFilter,
+			@RequestParam(name = "userPermDatasetCodeFilter", required = false) String userPermDatasetCodeFilter,
+			@RequestParam(name = "userEnablePendingFilter", required = false) Boolean userEnablePendingFilter) {
+
+		OrderingField orderBy = OrderingField.NAME;
+
+		PermPageModel permPageModel = new PermPageModel();
+		permPageModel.setUserNameFilter(userNameFilter);
+		permPageModel.setUserPermDatasetCodeFilter(userPermDatasetCodeFilter);
+		permPageModel.setUserEnablePendingFilter(userEnablePendingFilter);
+		permPageModel.setOrderBy(orderBy);
+
+		if (StringUtils.isBlank(userNameFilter)
+				&& StringUtils.isBlank(userPermDatasetCodeFilter)
+				&& (userEnablePendingFilter == null)) {
+			return permPageModel;
+		}
+
+		final int minUserNameLength = 2;
+		int userNameFilterLength = StringUtils.length(userNameFilter);
+
+		if ((userNameFilterLength > 0) && (userNameFilterLength < minUserNameLength)) {
+			return permPageModel;
+		}
+
+		List<EkiUserPermData> ekiUserPermissions = permissionService.getEkiUserPermissions(userNameFilter, userPermDatasetCodeFilter, userEnablePendingFilter, orderBy);
+
+		permPageModel.setEkiUserPermissions(ekiUserPermissions);
+
+		return permPageModel;
 	}
 }
