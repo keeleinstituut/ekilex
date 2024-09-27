ae9dc37602542b96d2ccae29e69297c5638669af
Martin Vaik <martin.vaik@twn.ee>
2024-09-27 10:21:11 +0300

    feat: made header name link to index

--- ekilex-frontend/src/components/header/header.tsx
+++ ekilex-frontend/src/components/header/header.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <div className="bg-eki-blue h-12 flex items-center px-4 gap-2 text-white">
-      <span className="text-sm">Ekilex</span>
+      <Link href='/' className="text-sm">Ekilex</Link>
       <span className="bg-eki-warning px-12 text-eki-blue font-bold">TEST</span>
     </div>
   );
