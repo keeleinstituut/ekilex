5a650308a028ca9db045fd86c191175737c50f25
Martin Vaik <martin.vaik@twn.ee>
2025-04-04 14:06:39 +0300

    feat(ekilex): add option to have multiple editors in one dialog

--- ekilex-app/src/main/resources/view/js/ekilex-dialog.js
+++ ekilex-app/src/main/resources/view/js/ekilex-dialog.js
@@ -46,6 +46,15 @@ $(function() {
 		})
 	}
 
+	$.fn.initMultipleEkiEditorDlgPlugin = function() {
+		return this.each(function() {
+			const obj = $(this);
+			obj.on('show.bs.modal', function() {
+				initMultipleEkiEditorDlg(obj);
+			})
+		})
+	}
+
 	$.fn.initEkiEditorDlgAndFocusPlugin = function() {
 		return this.each(function() {
 			const obj = $(this);
