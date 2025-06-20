9f0cc2c819dfff02825aff90edd1688ada8b721c
Martin Vaik <martin.vaik@twn.ee>
2025-06-20 10:53:28 +0300

    feat(ekilex): add new register page

--- ekilex-app/src/main/resources/view/js/ekilex-register.js
+++ ekilex-app/src/main/resources/view/js/ekilex-register.js
@@ -16,8 +16,9 @@ $.fn.registerClickPlugin = function() {
 $.fn.agreementChkPlugin = function() {
 	return this.each(function() {
 		const obj = $(this);
+		const input = obj.find('input').get(0);
 		obj.on('change', function() {
-			if (this.checked) {
+			if (input?.checked) {
 				$('#registerBtn').prop("disabled", false);
 			} else {
 				$('#registerBtn').prop("disabled", true);
