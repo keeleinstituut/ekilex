137a5551de3537e9148035f0a39cbbdf6db31339
Martin Laubre <martin.laubre@gmail.com>
2025-12-08 14:32:51 +0200

    more hacker protection

--- wordweb-app/src/main/java/eki/wordweb/web/controller/FeedbackController.java
+++ wordweb-app/src/main/java/eki/wordweb/web/controller/FeedbackController.java
@@ -1,8 +1,11 @@
 package eki.wordweb.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,8 +18,24 @@ import eki.wordweb.service.FeedbackService;
 @Controller
 public class FeedbackController implements WebConstant {
 
+	private static final Logger logger = LoggerFactory.getLogger(FeedbackController.class);
+
 	@Autowired
 	private FeedbackService feedbackService;
+
+	// dummy response for hackers
+	@GetMapping("/feed")
+	public String feedGet() {
+		logger.warn("Illegal attempt to access feedback form");
+		return "nothing";
+	}
+
+	// dummy response for hackers
+	@PostMapping("/feed")
+	public String feedPost() {
+		logger.warn("Illegal attempt to access feedback form");
+		return "nothing";
+	}
 
 	@PostMapping(FEEDBACK_URI)
 	@ResponseBody
