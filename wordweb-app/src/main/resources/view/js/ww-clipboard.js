e5adc66a24152ea36c00eaf581024ec8f1375b9c
Martin Vaik <martin.vaik@twn.ee>
2026-01-05 17:18:06 +0200

    feat(ww): add clipboard text announcing

--- wordweb-app/src/main/resources/view/js/ww-clipboard.js
+++ wordweb-app/src/main/resources/view/js/ww-clipboard.js
@@ -1,34 +1,40 @@
 let clipboardTimeout;
 
 function clipboardCloseCallback(event) {
-	if (!event.target.closest('.clipboard') && this.destroy) {
-		this.destroy();
-		document.removeEventListener('click', clipboardCloseCallback);
-		clearTimeout(clipboardTimeout);
-	}
+  if (!event.target.closest(".clipboard") && this.destroy) {
+    this.destroy();
+    document.removeEventListener("click", clipboardCloseCallback);
+    clearTimeout(clipboardTimeout);
+  }
 }
 
-$(document).on("click", ".clipboard__trigger", function() {
-	const button = this;
-	const popup = this.closest('.clipboard').querySelector('.clipboard__popup');
-	if (!popup) {
-		return;
+$(document).on("click", ".clipboard__trigger", function () {
+  const button = this;
+  const popup = this.closest(".clipboard").querySelector(".clipboard__popup");
+  if (!popup) {
+    return;
+  }
+  const popperInstance = new Popper(button, popup, {
+    placement: "bottom",
+    modifiers: [{ name: "offset", options: { offset: [24, 24] } }],
+  });
+  clearTimeout(clipboardTimeout);
+  const announcePrefix = popup.getAttribute("data-announce-prefix") ?? "";
+  const clipboardText = popup.getAttribute("data-clipboard-text");
+  try {
+    navigator.clipboard.writeText(clipboardText);
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+  if (window.announceForScreenReader) {
+		window.announceForScreenReader(announcePrefix + clipboardText);
 	}
-	const popperInstance = new Popper(button, popup, {
-		placement: 'bottom',
-		modifiers: [
-			{ name: 'offset', options: { offset: [24, 24] } }
-		]
-	});
-	clearTimeout(clipboardTimeout);
-	try {
-		navigator.clipboard.writeText(popup.getAttribute('data-clipboard-text'));
-	} catch (err) {
-		console.error('Failed to copy: ', err);
-	}
-	clipboardTimeout = setTimeout(() => {
-		popperInstance.destroy();
-		document.removeEventListener('click', clipboardCloseCallback);
-	}, 5000);
-	document.addEventListener('click', clipboardCloseCallback.bind(popperInstance));
+  clipboardTimeout = setTimeout(() => {
+    popperInstance.destroy();
+    document.removeEventListener("click", clipboardCloseCallback);
+  }, 5000);
+  document.addEventListener(
+    "click",
+    clipboardCloseCallback.bind(popperInstance)
+  );
 });
