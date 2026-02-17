313d777dcfeedf805094a5c70d4da3f6412d5adf
Martin Laubre <martin.laubre@gmail.com>
2026-02-17 13:37:51 +0200

    disabled security restrictions

--- wwexam-app/src/main/java/eki/wwexam/config/WebSecurityConfig.java
+++ wwexam-app/src/main/java/eki/wwexam/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import eki.common.web.interceptor.MutingHttpFirewall;
 
@@ -19,15 +18,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .antMatchers("/actuator", "/actuator/**")
-                .authenticated()
-                .requestMatchers(EndpointRequest.to(ShutdownEndpoint.class))
-                .hasRole("ACTUATOR_ADMIN")
-                .and()
-                .httpBasic(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
+		http
+				.authorizeHttpRequests()
+				.antMatchers("/actuator", "/actuator/**")
+				.authenticated()
+				.requestMatchers(EndpointRequest.to(ShutdownEndpoint.class))
+				.hasRole("ACTUATOR_ADMIN")
+				.and()
+				.httpBasic(withDefaults())
+				.headers(headers -> headers.frameOptions().disable())
+				.cors(cors -> cors.disable())
+				.csrf(csrf -> csrf.disable());
 	}
 
 	@Override
