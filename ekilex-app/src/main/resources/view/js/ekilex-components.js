c7cb0e0c5069d27ee9dffca5f03c0d2cb0cdccd8
Martin Vaik <martin.vaik@twn.ee>
2026-04-09 08:57:18 +0300

    chore(ekilex): move action dropdown

--- ekilex-app/src/main/resources/view/js/ekilex-components.js
+++ ekilex-app/src/main/resources/view/js/ekilex-components.js
@@ -1,3 +1,119 @@
+$.fn.actionDropdownHandler = function () {
+  $(this).each(function () {
+    const instance = new ActionDropdown($(this));
+    instance.init();
+  });
+};
+
+class ActionDropdown {
+  triggerClass = "action-dropdown__trigger";
+  menuClass = "action-dropdown__menu";
+  static clickOutsideInitialized = false;
+  static popperInstance;
+  static defaultOptions = {
+    triggerClass: "action-dropdown__trigger",
+    menuClass: "action-dropdown__menu",
+    placement: "bottom-end",
+  };
+
+  constructor(container, options = {}) {
+    this.container = container;
+    this.options = { ...ActionDropdown.defaultOptions, ...options };
+    this.triggerClass = this.options.triggerClass;
+    this.menuClass = this.options.menuClass;
+    this.trigger = container.find(`.${this.triggerClass}`);
+    this.menu = container.find(`.${this.menuClass}`);
+
+    if (!this.trigger.length) {
+      console.error(
+        "Could not find trigger for action dropdown: ",
+        this.container,
+      );
+    }
+    if (!this.menu.length) {
+      console.error(
+        "Could not find menu for action dropdown: ",
+        this.container,
+      );
+    }
+  }
+
+  init() {
+    this.bindDropdown();
+  }
+
+  bindDropdown() {
+    this.trigger.on("show.bs.dropdown", () => {
+      // Append to body because parent could have overflow clipped
+      this.menu.appendTo("body").addClass(`${this.menuClass}--visible`);
+      // This relies on popper being available, currently being added to window via bootstrap bundle
+      ActionDropdown.popperInstance = new Popper(
+        this.trigger.get(0),
+        this.menu.get(0),
+        {
+          placement: this.options.placement,
+          modifiers: {
+            preventOverflow: {
+              boundariesElement: "viewport",
+            },
+          },
+        },
+      );
+      this.trigger.attr("aria-expanded", true);
+      requestAnimationFrame(() => {
+        const firstChild = this.menu.children().first();
+        if (firstChild) {
+          firstChild.focus();
+        }
+      });
+    });
+
+    this.trigger.on("hide.bs.dropdown", () => {
+      if (!ActionDropdown.popperInstance) {
+        return;
+      }
+      const menu = ActionDropdown.popperInstance.popper;
+      const trigger = ActionDropdown.popperInstance.reference;
+      ActionDropdown.popperInstance.destroy();
+      ActionDropdown.popperInstance = null;
+
+      // Move menu back on close
+      $(menu)
+        .removeClass(`${this.menuClass}--visible`)
+        .insertAfter(this.trigger);
+      $(trigger).attr("aria-expanded", false);
+    });
+
+    this.trigger.on("click", () => {
+      let openNew = false;
+      if (this.trigger.attr("aria-expanded") === "false") {
+        openNew = true;
+      }
+      if (ActionDropdown.popperInstance) {
+        this.closeMenu();
+      }
+      if (openNew) {
+        requestAnimationFrame(() => {
+          this.trigger.trigger("show.bs.dropdown");
+        });
+      }
+    });
+
+    if (!ActionDropdown.clickOutsideInitialized) {
+      $(document).on("click", (e) => {
+        if (!e.target.classList.contains(this.triggerClass)) {
+          this.closeMenu();
+        }
+      });
+      ActionDropdown.clickOutsideInitialized = true;
+    }
+  }
+
+  closeMenu() {
+    $(`.${this.triggerClass}`).trigger("hide.bs.dropdown");
+  }
+}
+
 /**
  * Generic segmented switch: toggles [data-segment-panel] visibility
  * based on radio [data-segment-target] selection within a [data-segment-container].
