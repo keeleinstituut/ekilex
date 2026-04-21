2049a56d74ec0b54026f3858349cff108812f7ba
Martin Vaik <martin.vaik@twn.ee>
2026-04-21 17:57:01 +0300

    feat(ww): remove unneeded parts, add content to mobile homonym select

--- wordweb-app/src/main/resources/view/js/ww-common-search.js
+++ wordweb-app/src/main/resources/view/js/ww-common-search.js
@@ -13,44 +13,12 @@ $(document).ready(function() {
 	}
 });
 
-$(document).on("wordDetailsLoaded", function() {
-	addCollapseAriaAttributes();
-});
-
 $(document).on("click", "#homonymListToggleButton", function() {
-	$(".homonym-list").toggleClass("expand");
-});
-
-/*
-// TODO refactor or remove?
-$(document).on("click", ".homonym-item", function() {
-	$(".homonym-list-item").removeClass("selected last-selected");
-	$(".homonym-item:first").removeClass("animation-target").dequeue();
-	$(this).parents(".homonym-list-item").addClass("selected last-selected");
 	const homonymList = $(".homonym-list");
-	if ($(window).width() >= windowWidthTreshold) {
-		const bodyParentPositionLeft = $(
-			".homonym-list-item.selected .homonym__body",
-		)
-			.parent()
-			.position()?.left;
-		if (!bodyParentPositionLeft) {
-			return;
-		}
-		homonymList.animate(
-			{
-				scrollLeft:
-					bodyParentPositionLeft -
-					$("#word-details, .os-results").offset().left +
-					homonymList.scrollLeft(),
-			},
-			200,
-		);
-	}
-	setSelectedHomonymValueForMobile($(this).html());
-	$(".homonym-list").removeClass("expand");
+	const isExpanded = homonymList.hasClass("expand");
+	$(this).attr("aria-expanded", !isExpanded);
+	homonymList.toggleClass("expand");
 });
-*/
 
 $(document).on("click", "#clear-search-btn", function(e) {
 	const searchInput = $("input[name='searchWordValue']");
@@ -563,47 +531,6 @@ function initLangDropdown() {
 	});
 }
 
-/*
-// TODO refactor or remove?
-function initHomonymList() {
-	const selectedHomonymItem = getSelectedHomonym();
-	if (document.getElementsByClassName("os-results").length === 0) {
-		selectedHomonymItem
-			.delay(500)
-			.queue(function() { })
-			.trigger("click");
-		selectedHomonymItem.addClass("animation-target");
-	}
-
-	setSelectedHomonymValueForMobile(selectedHomonymItem.html());
-}
-
-// TODO refactor or remove?
-function getSelectedHomonym() {
-	let selectedHomonymItem = $(".homonym-item")
-		.filter(function() {
-			const isHomonymSelected = $(this)
-				.closest("form")
-				.find("input[name='word-selected']")
-				.val();
-			return isHomonymSelected === "true";
-		})
-		.filter(":first");
-	if (selectedHomonymItem.get().length == 0) {
-		selectedHomonymItem = $(".homonym-item:first");
-	}
-	return selectedHomonymItem;
-}
-
-// TODO refactor or remove?
-function setSelectedHomonymValueForMobile(inputHTML) {
-	const isMultiHomonym = $(".homonym-item").length > 1;
-	if (isMultiHomonym) {
-		$("#homonymListToggleButton").html(inputHTML);
-	}
-}
-*/
-
 function initWordDetails() {
 	fetchCorpSentences();
 	fetchCorpTranslations();
@@ -619,8 +546,7 @@ function initWordDetails() {
 		closeText: '<i class="fas fa-times"></i>'
 	});
 	$("#mainContentArea").removeClass("loading");
-	const event = new CustomEvent("wordDetailsLoaded");
-	document.dispatchEvent(event);
+	addCollapseAriaAttributes();
 }
 
 function addCollapseAriaAttributes() {
--- wordweb-app/src/main/resources/view/js/ww-search.js
+++ wordweb-app/src/main/resources/view/js/ww-search.js
@@ -28,40 +28,7 @@ $(document).on("click", ".btn-ellipsis", function(e) {
 	$(this).tooltip('hide');
 });
 
-/*
-// TODO refactor or remove
-function fetchDetails(wordId, wordSelectUrl) {
-	const wordDetailsUrlWithParams = wordDetailsUrl + "/" + wordId;
-	$.get(wordDetailsUrlWithParams).done(function(data) {
-		const wordDetailsWrapper = $('#word-details');
-		if (!wordDetailsWrapper.length) {
-			console.error("Unable to find word details wrapper with id 'word-details'")
-		}
-		wordDetailsWrapper.replaceWith(data);
-		fetchCorpSentences();
-		fetchCorpTranslations();
-		updateBrowserHistory(wordSelectUrl);
-		setHomonymNrVisibility();
-		$('#word-details [data-toggle="tooltip"], [data-tooltip="tooltip"]').tooltip();
-		$('[data-toggle="popover"]').popover({
-			placement: 'top'
-		});
-		const lightbox = new SimpleLightbox('.gallery-image', {
-			history: false,
-			captionPosition: 'outside',
-			navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
-			closeText: '<i class="fas fa-times"></i>'
-		});
-		$("#mainContentArea").removeClass("loading");
-		const event = new CustomEvent("wordDetailsLoaded");
-		document.dispatchEvent(event);
-	}).fail(function(data) {
-		alert(messages.search_failure);
-		$("#mainContentArea").removeClass("loading");
-	})
-}
-*/
-
+// TODO: might be removable with new search logic?
 function updateBrowserHistory(wordSelectUrl) {
 	if (currentWord.indexOf('/') !== -1) {
 		wordSelectUrl = wordSelectUrl.replace(currentWord, encodeURIComponent(currentWord));
@@ -99,15 +66,6 @@ $(document).on("click", ".menu-btn", function() {
 $(document).on("click", "button[name='expand-btn']", function() {
 	$(this).parent().find(".collapsable[data-collapse='true']").fadeToggle("slow", "linear");
 });
-
-/* TODO remove?
-$(document).on("click", "button[id^='word-details-link']", function() {
-	const wordWrapperForm = $(this).closest("form");
-	const wordId = wordWrapperForm.children("[name='word-id']").val();
-	const wordSelectUrl = wordWrapperForm.children("[name='word-select-url']").val();
-	fetchDetails(wordId, wordSelectUrl);
-});
-*/
 
 $(document).on("click", "a[id^='feedback-link']", function() {
 	$("button[data-target='#feedback-modal']").trigger('click');
