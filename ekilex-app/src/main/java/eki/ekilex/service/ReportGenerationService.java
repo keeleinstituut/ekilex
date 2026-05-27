1850d1ffbdcc0e54d870d1dda7fc62e129ef2fea
Henri Lindpere <henri.lindpere@tripledev.ee>
2026-05-27 14:16:34 +0300

    reports cleanup

--- ekilex-app/src/main/java/eki/ekilex/service/ReportGenerationService.java
+++ ekilex-app/src/main/java/eki/ekilex/service/ReportGenerationService.java
@@ -25,80 +25,80 @@ import eki.ekilex.service.db.TermDatasetReportDbService;
 @Component
 public class ReportGenerationService {
 
-  private static final Logger logger = LoggerFactory.getLogger(ReportGenerationService.class);
+	private static final Logger logger = LoggerFactory.getLogger(ReportGenerationService.class);
 
-  @Autowired
-  private ReportDbService reportDbService;
+	@Autowired
+	private ReportDbService reportDbService;
 
-  @Autowired
-  private TermDatasetReportDbService termDatasetReportDbService;
+	@Autowired
+	private TermDatasetReportDbService termDatasetReportDbService;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-  @Transactional(rollbackFor = Exception.class)
-  public void generate(Long reportId, ReportType reportType, ReportParameters parameters) {
+	@Transactional(rollbackFor = Exception.class)
+	public void generate(Long reportId, ReportType reportType, ReportParameters parameters) {
 
-    try {
-      String content = buildContent(reportType, parameters);
-      reportDbService.updateReportCompleted(reportId, content);
-    } catch (Exception e) {
-      logger.error("Report generation failed for report with ID \"{}\"", reportId, e);
-      reportDbService.updateReportFailed(reportId);
-    }
-  }
+		try {
+			String content = buildContent(reportType, parameters);
+			reportDbService.updateReportCompleted(reportId, content);
+		} catch (Exception e) {
+			logger.error("Report generation failed for report with ID \"{}\"", reportId, e);
+			reportDbService.updateReportFailed(reportId);
+		}
+	}
 
-  private String buildContent(ReportType reportType, ReportParameters parameters) throws Exception {
+	private String buildContent(ReportType reportType, ReportParameters parameters) throws Exception {
 
-    String content = switch (reportType) {
-      case TERM_DATASET -> buildTermDatasetContent(parameters);
-      default -> throw new IllegalArgumentException("Report generation not implemented for type: " + reportType);
-    };
+		String content = switch (reportType) {
+		case TERM_DATASET -> buildTermDatasetContent(parameters);
+		default -> throw new IllegalArgumentException("Report generation not implemented for type: " + reportType);
+		};
 
-    return content;
-  }
+		return content;
+	}
 
-  private String buildTermDatasetContent(ReportParameters parameters) throws Exception {
+	private String buildTermDatasetContent(ReportParameters parameters) throws Exception {
 
-    TermDatasetReportParameters reportParameters = (TermDatasetReportParameters) parameters;
+		TermDatasetReportParameters reportParameters = (TermDatasetReportParameters) parameters;
 
-    List<String> datasetCodes = reportParameters.getDatasetCodes();
-    LocalDateTime from = reportParameters.getDateFrom().atStartOfDay();
-    LocalDateTime until = reportParameters.getDateUntil().plusDays(1).atStartOfDay();
+		List<String> datasetCodes = reportParameters.getDatasetCodes();
+		LocalDateTime from = reportParameters.getDateFrom().atStartOfDay();
+		LocalDateTime until = reportParameters.getDateUntil().plusDays(1).atStartOfDay();
 
-    List<Dataset> datasets = termDatasetReportDbService.getDatasets(datasetCodes);
-    Map<String, Integer> publicMeaningCounts = termDatasetReportDbService.getPublicMeaningCounts(datasetCodes);
-    Map<String, Integer> allMeaningCounts = termDatasetReportDbService.getAllMeaningCounts(datasetCodes);
-    Map<String, Integer> publicTermCounts = termDatasetReportDbService.getPublicTermCounts(datasetCodes);
-    Map<String, Integer> allTermCounts = termDatasetReportDbService.getAllTermCounts(datasetCodes);
-    Map<String, Integer> createMeaningCounts = termDatasetReportDbService.getCreateMeaningCounts(datasetCodes, from, until);
-    Map<String, Integer> updateMeaningCounts = termDatasetReportDbService.getUpdateMeaningCounts(datasetCodes, from, until);
+		List<Dataset> datasets = termDatasetReportDbService.getDatasets(datasetCodes);
+		Map<String, Integer> publicMeaningCounts = termDatasetReportDbService.getPublicMeaningCounts(datasetCodes);
+		Map<String, Integer> allMeaningCounts = termDatasetReportDbService.getAllMeaningCounts(datasetCodes);
+		Map<String, Integer> publicTermCounts = termDatasetReportDbService.getPublicTermCounts(datasetCodes);
+		Map<String, Integer> allTermCounts = termDatasetReportDbService.getAllTermCounts(datasetCodes);
+		Map<String, Integer> createMeaningCounts = termDatasetReportDbService.getCreateMeaningCounts(datasetCodes, from, until);
+		Map<String, Integer> updateMeaningCounts = termDatasetReportDbService.getUpdateMeaningCounts(datasetCodes, from, until);
 
-    List<TermDatasetReportRow> datasetRows = new ArrayList<>();
+		List<TermDatasetReportRow> datasetRows = new ArrayList<>();
 
-    for (Dataset dataset : datasets) {
-      String datasetCode = dataset.getCode();
+		for (Dataset dataset : datasets) {
+			String datasetCode = dataset.getCode();
 
-      TermDatasetReportRow row = new TermDatasetReportRow();
-      row.setDatasetCode(datasetCode);
-      row.setDatasetName(dataset.getName());
+			TermDatasetReportRow row = new TermDatasetReportRow();
+			row.setDatasetCode(datasetCode);
+			row.setDatasetName(dataset.getName());
 
-      row.setPublicMeaningCount(publicMeaningCounts.getOrDefault(datasetCode, 0));
-      row.setAllMeaningCount(allMeaningCounts.getOrDefault(datasetCode, 0));
-      row.setPublicTermCount(publicTermCounts.getOrDefault(datasetCode, 0));
-      row.setAllTermCount(allTermCounts.getOrDefault(datasetCode, 0));
-      row.setCreateMeaningCount(createMeaningCounts.getOrDefault(datasetCode, 0));
-      row.setUpdateMeaningCount(updateMeaningCounts.getOrDefault(datasetCode, 0));
+			row.setPublicMeaningCount(publicMeaningCounts.getOrDefault(datasetCode, 0));
+			row.setAllMeaningCount(allMeaningCounts.getOrDefault(datasetCode, 0));
+			row.setPublicTermCount(publicTermCounts.getOrDefault(datasetCode, 0));
+			row.setAllTermCount(allTermCounts.getOrDefault(datasetCode, 0));
+			row.setCreateMeaningCount(createMeaningCounts.getOrDefault(datasetCode, 0));
+			row.setUpdateMeaningCount(updateMeaningCounts.getOrDefault(datasetCode, 0));
 
-      datasetRows.add(row);
-    }
+			datasetRows.add(row);
+		}
 
-    TermDatasetReportContent content = new TermDatasetReportContent();
-    content.setParameters(reportParameters);
-    content.setRows(datasetRows);
+		TermDatasetReportContent content = new TermDatasetReportContent();
+		content.setParameters(reportParameters);
+		content.setRows(datasetRows);
 
-    String contentJson = objectMapper.writeValueAsString(content);
-    return contentJson;
-  }
+		String contentJson = objectMapper.writeValueAsString(content);
+		return contentJson;
+	}
 
 }
--- ekilex-app/src/main/java/eki/ekilex/service/db/TermDatasetReportDbService.java
+++ ekilex-app/src/main/java/eki/ekilex/service/db/TermDatasetReportDbService.java
@@ -30,240 +30,240 @@ import eki.ekilex.data.db.main.tables.Word;
 @Component
 public class TermDatasetReportDbService {
 
-  @Autowired
-  private DSLContext mainDb;
+	@Autowired
+	private DSLContext mainDb;
 
-  public List<eki.ekilex.data.Dataset> getDatasets(List<String> datasetCodes) {
+	public List<eki.ekilex.data.Dataset> getDatasets(List<String> datasetCodes) {
 
-    return mainDb
-        .select(DATASET.CODE, DATASET.NAME)
-        .from(DATASET)
-        .where(DATASET.CODE.in(datasetCodes))
-        .orderBy(DATASET.NAME)
-        .fetchInto(eki.ekilex.data.Dataset.class);
-  }
+		return mainDb
+				.select(DATASET.CODE, DATASET.NAME)
+				.from(DATASET)
+				.where(DATASET.CODE.in(datasetCodes))
+				.orderBy(DATASET.NAME)
+				.fetchInto(eki.ekilex.data.Dataset.class);
+	}
 
-  public Map<String, Integer> getPublicMeaningCounts(List<String> datasetCodes) {
+	public Map<String, Integer> getPublicMeaningCounts(List<String> datasetCodes) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Meaning m = MEANING.as("m");
-    Word w = WORD.as("w");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Meaning m = MEANING.as("m");
+		Word w = WORD.as("w");
 
-    Field<Integer> publicMeaningCount = DSL.selectCount()
-        .from(m)
-        .where(DSL.exists(
-            DSL.selectOne()
-                .from(l, w)
-                .where(
-                    l.MEANING_ID.eq(m.ID)
-                        .and(l.WORD_ID.eq(w.ID))
-                        .and(l.DATASET_CODE.eq(ds.CODE))
-                        .and(l.IS_WORD.isTrue())
-                        .and(l.IS_PUBLIC.isTrue())
-                        .and(w.IS_PUBLIC.isTrue()))))
-        .asField();
+		Field<Integer> publicMeaningCount = DSL.selectCount()
+				.from(m)
+				.where(DSL.exists(
+						DSL.selectOne()
+								.from(l, w)
+								.where(
+										l.MEANING_ID.eq(m.ID)
+												.and(l.WORD_ID.eq(w.ID))
+												.and(l.DATASET_CODE.eq(ds.CODE))
+												.and(l.IS_WORD.isTrue())
+												.and(l.IS_PUBLIC.isTrue())
+												.and(w.IS_PUBLIC.isTrue()))))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, publicMeaningCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, publicMeaningCount);
-  }
+		return mainDb
+				.select(ds.CODE, publicMeaningCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, publicMeaningCount);
+	}
 
-  public Map<String, Integer> getAllMeaningCounts(List<String> datasetCodes) {
+	public Map<String, Integer> getAllMeaningCounts(List<String> datasetCodes) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Meaning m = MEANING.as("m");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Meaning m = MEANING.as("m");
 
-    Field<Integer> allMeaningCount = DSL.selectCount()
-        .from(m)
-        .where(DSL.exists(
-            DSL.selectOne()
-                .from(l)
-                .where(
-                    l.MEANING_ID.eq(m.ID)
-                        .and(l.DATASET_CODE.eq(ds.CODE))
-                        .and(l.IS_WORD.isTrue()))))
-        .asField();
+		Field<Integer> allMeaningCount = DSL.selectCount()
+				.from(m)
+				.where(DSL.exists(
+						DSL.selectOne()
+								.from(l)
+								.where(
+										l.MEANING_ID.eq(m.ID)
+												.and(l.DATASET_CODE.eq(ds.CODE))
+												.and(l.IS_WORD.isTrue()))))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, allMeaningCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, allMeaningCount);
-  }
+		return mainDb
+				.select(ds.CODE, allMeaningCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, allMeaningCount);
+	}
 
-  public Map<String, Integer> getPublicTermCounts(List<String> datasetCodes) {
+	public Map<String, Integer> getPublicTermCounts(List<String> datasetCodes) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Word w = WORD.as("w");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Word w = WORD.as("w");
 
-    Field<Integer> publicTermCount = DSL.selectCount()
-        .from(w)
-        .where(
-            w.IS_PUBLIC.isTrue()
-                .and(DSL.exists(
-                    DSL.selectOne()
-                        .from(l)
-                        .where(
-                            l.WORD_ID.eq(w.ID)
-                                .and(l.DATASET_CODE.eq(ds.CODE))
-                                .and(l.IS_WORD.isTrue())
-                                .and(l.IS_PUBLIC.isTrue())))))
-        .asField();
+		Field<Integer> publicTermCount = DSL.selectCount()
+				.from(w)
+				.where(
+						w.IS_PUBLIC.isTrue()
+								.and(DSL.exists(
+										DSL.selectOne()
+												.from(l)
+												.where(
+														l.WORD_ID.eq(w.ID)
+																.and(l.DATASET_CODE.eq(ds.CODE))
+																.and(l.IS_WORD.isTrue())
+																.and(l.IS_PUBLIC.isTrue())))))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, publicTermCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, publicTermCount);
-  }
+		return mainDb
+				.select(ds.CODE, publicTermCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, publicTermCount);
+	}
 
-  public Map<String, Integer> getAllTermCounts(List<String> datasetCodes) {
+	public Map<String, Integer> getAllTermCounts(List<String> datasetCodes) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Word w = WORD.as("w");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Word w = WORD.as("w");
 
-    Field<Integer> allTermCount = DSL.selectCount()
-        .from(w)
-        .where(DSL.exists(
-            DSL.selectOne()
-                .from(l)
-                .where(
-                    l.WORD_ID.eq(w.ID)
-                        .and(l.DATASET_CODE.eq(ds.CODE))
-                        .and(l.IS_WORD.isTrue()))))
-        .asField();
+		Field<Integer> allTermCount = DSL.selectCount()
+				.from(w)
+				.where(DSL.exists(
+						DSL.selectOne()
+								.from(l)
+								.where(
+										l.WORD_ID.eq(w.ID)
+												.and(l.DATASET_CODE.eq(ds.CODE))
+												.and(l.IS_WORD.isTrue()))))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, allTermCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, allTermCount);
-  }
+		return mainDb
+				.select(ds.CODE, allTermCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, allTermCount);
+	}
 
-  public Map<String, Integer> getCreateMeaningCounts(List<String> datasetCodes, LocalDateTime from, LocalDateTime until) {
+	public Map<String, Integer> getCreateMeaningCounts(List<String> datasetCodes, LocalDateTime from, LocalDateTime until) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Meaning m = MEANING.as("m");
-    Word w = WORD.as("w");
-    ActivityLog al = ACTIVITY_LOG.as("al");
-    MeaningActivityLog mal = MEANING_ACTIVITY_LOG.as("mal");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Meaning m = MEANING.as("m");
+		Word w = WORD.as("w");
+		ActivityLog al = ACTIVITY_LOG.as("al");
+		MeaningActivityLog mal = MEANING_ACTIVITY_LOG.as("mal");
 
-    Condition publicTermExistsCondition = DSL.exists(
-        DSL.selectOne()
-            .from(l, w)
-            .where(
-                l.MEANING_ID.eq(m.ID)
-                    .and(l.WORD_ID.eq(w.ID))
-                    .and(l.DATASET_CODE.eq(ds.CODE))
-                    .and(l.IS_WORD.isTrue())
-                    .and(l.IS_PUBLIC.isTrue())
-                    .and(w.IS_PUBLIC.isTrue())));
+		Condition publicTermExistsCondition = DSL.exists(
+				DSL.selectOne()
+						.from(l, w)
+						.where(
+								l.MEANING_ID.eq(m.ID)
+										.and(l.WORD_ID.eq(w.ID))
+										.and(l.DATASET_CODE.eq(ds.CODE))
+										.and(l.IS_WORD.isTrue())
+										.and(l.IS_PUBLIC.isTrue())
+										.and(w.IS_PUBLIC.isTrue())));
 
-    Field<LocalDateTime> firstEventOn = DSL
-        .select(al.EVENT_ON)
-        .from(mal, al)
-        .where(
-            mal.MEANING_ID.eq(m.ID)
-                .and(mal.ACTIVITY_LOG_ID.eq(al.ID)))
-        .orderBy(al.EVENT_ON.asc())
-        .limit(1)
-        .asField("first_event_on");
+		Field<LocalDateTime> firstEventOn = DSL
+				.select(al.EVENT_ON)
+				.from(mal, al)
+				.where(
+						mal.MEANING_ID.eq(m.ID)
+								.and(mal.ACTIVITY_LOG_ID.eq(al.ID)))
+				.orderBy(al.EVENT_ON.asc())
+				.limit(1)
+				.asField("first_event_on");
 
-    Field<Integer> createMeaningCount = DSL.selectCount()
-        .from(DSL.select(m.ID, firstEventOn)
-            .from(m)
-            .where(publicTermExistsCondition)
-            .asTable("m"))
-        .where(firstEventOn.ge(from))
-        .and(firstEventOn.lt(until))
-        .asField();
+		Field<Integer> createMeaningCount = DSL.selectCount()
+				.from(DSL.select(m.ID, firstEventOn)
+						.from(m)
+						.where(publicTermExistsCondition)
+						.asTable("m"))
+				.where(firstEventOn.ge(from))
+				.and(firstEventOn.lt(until))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, createMeaningCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, createMeaningCount);
-  }
+		return mainDb
+				.select(ds.CODE, createMeaningCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, createMeaningCount);
+	}
 
-  public Map<String, Integer> getUpdateMeaningCounts(List<String> datasetCodes, LocalDateTime from, LocalDateTime until) {
+	public Map<String, Integer> getUpdateMeaningCounts(List<String> datasetCodes, LocalDateTime from, LocalDateTime until) {
 
-    Dataset ds = DATASET.as("ds");
-    Lexeme l = LEXEME.as("l");
-    Meaning m = MEANING.as("m");
-    Word w = WORD.as("w");
-    ActivityLog al = ACTIVITY_LOG.as("al");
-    MeaningActivityLog mal = MEANING_ACTIVITY_LOG.as("mal");
+		Dataset ds = DATASET.as("ds");
+		Lexeme l = LEXEME.as("l");
+		Meaning m = MEANING.as("m");
+		Word w = WORD.as("w");
+		ActivityLog al = ACTIVITY_LOG.as("al");
+		MeaningActivityLog mal = MEANING_ACTIVITY_LOG.as("mal");
 
-    Condition meaningUpdatedCondition = getMeaningUpdatedInPeriodCondition(m, al, mal, from, until);
+		Condition meaningUpdatedCondition = getMeaningUpdatedInPeriodCondition(m, al, mal, from, until);
 
-    Field<Integer> updateMeaningCount = DSL.selectCount()
-        .from(m)
-        .where(DSL.exists(
-                DSL.selectOne()
-                    .from(l, w)
-                    .where(
-                        l.MEANING_ID.eq(m.ID)
-                            .and(l.WORD_ID.eq(w.ID))
-                            .and(l.DATASET_CODE.eq(ds.CODE))
-                            .and(l.IS_WORD.isTrue())
-                            .and(l.IS_PUBLIC.isTrue())
-                            .and(w.IS_PUBLIC.isTrue())))
-            .and(meaningUpdatedCondition))
-        .asField();
+		Field<Integer> updateMeaningCount = DSL.selectCount()
+				.from(m)
+				.where(DSL.exists(
+						DSL.selectOne()
+								.from(l, w)
+								.where(
+										l.MEANING_ID.eq(m.ID)
+												.and(l.WORD_ID.eq(w.ID))
+												.and(l.DATASET_CODE.eq(ds.CODE))
+												.and(l.IS_WORD.isTrue())
+												.and(l.IS_PUBLIC.isTrue())
+												.and(w.IS_PUBLIC.isTrue())))
+						.and(meaningUpdatedCondition))
+				.asField();
 
-    return mainDb
-        .select(ds.CODE, updateMeaningCount)
-        .from(ds)
-        .where(ds.CODE.in(datasetCodes))
-        .fetchMap(ds.CODE, updateMeaningCount);
-  }
+		return mainDb
+				.select(ds.CODE, updateMeaningCount)
+				.from(ds)
+				.where(ds.CODE.in(datasetCodes))
+				.fetchMap(ds.CODE, updateMeaningCount);
+	}
 
-  private Condition getMeaningUpdatedInPeriodCondition(
-      Meaning m, ActivityLog al, MeaningActivityLog mal, LocalDateTime from, LocalDateTime until) {
+	private Condition getMeaningUpdatedInPeriodCondition(
+			Meaning m, ActivityLog al, MeaningActivityLog mal, LocalDateTime from, LocalDateTime until) {
 
-    return DSL.or(
-        DSL.exists(
-            DSL.selectOne()
-                .from(mal, al)
-                .where(
-                    mal.MEANING_ID.eq(m.ID)
-                        .and(mal.ACTIVITY_LOG_ID.eq(al.ID))
-                        .and(al.OWNER_NAME.in(
-                            ActivityOwner.MEANING.name(),
-                            ActivityOwner.LEXEME.name()))
-                        .and(al.EVENT_ON.ge(from))
-                        .and(al.EVENT_ON.lt(until)))),
-        DSL.exists(
-            DSL.selectOne()
-                .from(mal, al)
-                .where(
-                    mal.MEANING_ID.eq(m.ID)
-                        .and(mal.ACTIVITY_LOG_ID.eq(al.ID))
-                        .and(al.OWNER_NAME.eq(ActivityOwner.WORD.name()))
-                        .and(al.ENTITY_NAME.notIn(
-                            ActivityEntity.GRAMMAR.name(),
-                            ActivityEntity.WORD_TYPE.name(),
-                            ActivityEntity.WORD_TAG.name(),
-                            ActivityEntity.WORD_NOTE.name(),
-                            ActivityEntity.WORD_RELATION.name(),
-                            ActivityEntity.WORD_RELATION_GROUP_MEMBER.name(),
-                            ActivityEntity.WORD_ETYMOLOGY.name(),
-                            ActivityEntity.WORD_OS_MORPH.name(),
-                            ActivityEntity.WORD_OS_USAGE.name(),
-                            ActivityEntity.PARADIGM.name(),
-                            ActivityEntity.FORM.name(),
-                            ActivityEntity.WORD_EKI_RECOMMENDATION.name(),
-                            ActivityEntity.TAG.name(),
-                            ActivityEntity.PUBLISHING.name()))
-                        .and(al.FUNCT_NAME.notLike("%join%"))
-                        .and(al.EVENT_ON.ge(from))
-                        .and(al.EVENT_ON.lt(until)))));
-  }
+		return DSL.or(
+				DSL.exists(
+						DSL.selectOne()
+								.from(mal, al)
+								.where(
+										mal.MEANING_ID.eq(m.ID)
+												.and(mal.ACTIVITY_LOG_ID.eq(al.ID))
+												.and(al.OWNER_NAME.in(
+														ActivityOwner.MEANING.name(),
+														ActivityOwner.LEXEME.name()))
+												.and(al.EVENT_ON.ge(from))
+												.and(al.EVENT_ON.lt(until)))),
+				DSL.exists(
+						DSL.selectOne()
+								.from(mal, al)
+								.where(
+										mal.MEANING_ID.eq(m.ID)
+												.and(mal.ACTIVITY_LOG_ID.eq(al.ID))
+												.and(al.OWNER_NAME.eq(ActivityOwner.WORD.name()))
+												.and(al.ENTITY_NAME.notIn(
+														ActivityEntity.GRAMMAR.name(),
+														ActivityEntity.WORD_TYPE.name(),
+														ActivityEntity.WORD_TAG.name(),
+														ActivityEntity.WORD_NOTE.name(),
+														ActivityEntity.WORD_RELATION.name(),
+														ActivityEntity.WORD_RELATION_GROUP_MEMBER.name(),
+														ActivityEntity.WORD_ETYMOLOGY.name(),
+														ActivityEntity.WORD_OS_MORPH.name(),
+														ActivityEntity.WORD_OS_USAGE.name(),
+														ActivityEntity.PARADIGM.name(),
+														ActivityEntity.FORM.name(),
+														ActivityEntity.WORD_EKI_RECOMMENDATION.name(),
+														ActivityEntity.TAG.name(),
+														ActivityEntity.PUBLISHING.name()))
+												.and(al.FUNCT_NAME.notLike("%join%"))
+												.and(al.EVENT_ON.ge(from))
+												.and(al.EVENT_ON.lt(until)))));
+	}
 }
